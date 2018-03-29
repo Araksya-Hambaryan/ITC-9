ca5d1b656eec082188d8bfbb5cc1bbc3dee9aa1f
home <gevor.antonyan94@gmail.com>
2018-03-29 23:29:38 +0400

    all homeworks c++

---
+++ Gevorg_Antonyan/Homework/26.03/calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main() {
+    float num1;
+    char sim;
+    float num2;
+    float k = 0;
+    std::cout << "mutqagrel gumareliner" << std::endl;
+    std::cin >> num1 >> sim >> num2;
+    if('+' == sim) {
+        k = num1 + num2;
+    }
+    if('-' == sim) {
+        k = num1 - num2;
+    }
+    if('*' == sim) {
+        k = num1 * num2;
+    }
+    if('/' == sim) {
+        if(0 == num2) {
+            std::cout << "error";
+        }else{
+            k = num1 / num2;
+        }
+    }
+    std::cout << num1 << sim << num2 << "=" << k;
+    return 0;
+}
+   
