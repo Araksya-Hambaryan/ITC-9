da1e1f20a40fb724e9da04e55e0cbc0a5ed30a80
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-03-29 13:00:11 +0400

    Draft

---
+++ Anush_Ghazaryan/test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<String>
+int mian() {
+
+	std::cout << "Input the data of the birthday : ";
+	string  birthday;
+	
+	int result = birthday / 10;
+	std::cout << result;
+return 0;
+}
