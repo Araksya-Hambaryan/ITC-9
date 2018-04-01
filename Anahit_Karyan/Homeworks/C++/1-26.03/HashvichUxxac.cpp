add596e478032c17ce44c28ebab60d43093a4338
AnahitKaryan <1anahit1@mai.ru>
2018-04-01 23:08:13 +0400

    Hashvi uxxac

---
+++ Anahit_Karyan/Homeworks/C++/1-26.03/HashvichUxxac.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main(){
+         int tiv1;
+         int tiv2;
+         char gorcoxutyun;
+         std::cout << "nermucel tiv1 gorcoxutyuv tiv2" << std::endl;
+         std::cin >> tiv1 >>gorcoxutyun >> tiv2 ;
+         if (gorcoxutyun == '+') {
+        	 std::cout << "=" << tiv1 + tiv2;
+         } else if (gorcoxutyun == '-') {
+        	 std::cout << "=" << tiv1 - tiv2;
+         } else if (gorcoxutyun == '*') {
+        	 std::cout << "=" << tiv1 * tiv2;
+         } else  if (gorcoxutyun == '/') {
+        	 if (tiv2 == 0){
+        		 std::cout << "0-i bajanel chi tuylatrvum";
+		 } else {
+        		 std::cout << "=" << tiv1 / tiv2;
+        	 }
+         }
+ return 0;
+ }
