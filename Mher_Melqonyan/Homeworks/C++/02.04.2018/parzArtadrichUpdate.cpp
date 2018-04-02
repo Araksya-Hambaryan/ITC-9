ba43bebf923c0629e8746951e7f782c2bf0f5c85
mmelqonyan <mmelqonyan1990@gmail.com>
2018-04-03 03:42:35 +0400

    parzArtadrichUpdate

---
+++ Mher_Melqonyan/Homeworks/C++/02.04.2018/parzArtadrichUpdate.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main() {
+    int tiv;
+    std::cout << "nermuceq tiv@" << std::endl;
+    std::cin >> tiv;
+    std::cout << "parz atradrichner@ klinen" << std::endl;
+    for(int i = 1; i <=  tiv ; ++i) {
+        if(0 == tiv % i) {
+            tiv /= i;
+            std::cout << i <<std::endl;
+            
+        }
+    }
+    return 0;
+}
