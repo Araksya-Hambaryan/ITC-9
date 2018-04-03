b678e0e98ca9e6604734e64883a4b2ccf5144809
Meliq <37870443+Meliq@users.noreply.github.com>
2018-04-03 12:16:47 +0300

    Update zam.cpp

--- Meliq_Melqonyan/Homeworks/c++/5.02.04.18/zam.cpp
+++ Meliq_Melqonyan/Homeworks/c++/5.02.04.18/zam.cpp
@@ -5,6 +5,12 @@ int main() {
 	std::cin >> zam1 >> rope1;
 	std::cout << "nermuceq verjnakan zam@::" << '\n';
 	std::cin >> zam2 >> rope2;
+	if(zam1 > zam2) {
+		zma2 = zam2 + 12;	
+	}
+	if(zam 1 = zam2 && rope1 > rope2) {
+		zam2 = zam2 + 12;	
+	}
 	if(rope1 < 7) {
 		pop++;
 	}
