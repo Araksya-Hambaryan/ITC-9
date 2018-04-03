fb43b0d6aae378ccdb9f2bf83aa5c51cf50c09f9
unknown <vahan.hovhannisyan.1997@gmail.com>
2018-04-03 12:11:41 +0400

    Improved Simple common dividers

--- Vahan_Hovhannisyan/Homework/5-02.04.2018/PrimeFactorsNonMutual.cpp
+++ Vahan_Hovhannisyan/Homework/5-02.04.2018/PrimeFactorsNonMutual.cpp
@@ -1,30 +1,39 @@
 ﻿// Program to print all prime factors
 #include <iostream> 
 #include <cmath> 
-#include <stack>
 
-  std::stack < int > primeFactorsInStack;
 // A function to print all prime factors of a given number n
-void primeFactors(int n) {
+void primeFactors(int n, int n2) {
+  int number = n;
   // Print the number of 2s that divide n
+  bool typed = false;
   while (n % 2 == 0) {
+
+    if (n2 % 2 != 0 && !typed) {
+      std::cout << "2 ";
+      typed = true;
+    } else if (n2 % 2 == 0) {
+      n2 = n2 / 2;
+    }
     n = n / 2;
-
-    primeFactorsInStack.push(2);
-
   }
 
   // n must be odd at this point.  So we can skip 
   // one element (Note i = i +2)
-  for (int i = 3; i <= sqrt(n); i = i + 2) {
+  for (int i = 3; i <= sqrt(number); i = i + 2) {
     // While i divides n, print i and divide n
-
+    int previous = 0;
     while (n % i == 0) {
 
-      primeFactorsInStack.push(i);
+      if ((n2 % i != 0) && (n != previous / i)) {
+        std::cout << i << " ";
+      }
 
+      previous = n;
       n = n / i;
-
+      if (n2 % i == 0) {
+        n2 = n2 / i;
+      }
     }
 
   }
@@ -32,7 +41,7 @@ void primeFactors(int n) {
   // This condition is to handle the case when n 
   // is a prime number greater than 2
   if (n > 2) {
-    primeFactorsInStack.push(n);
+    std::cout << n << " ";
 
   }
 }
@@ -52,17 +61,7 @@ int main() {
   inputNumber(n2);
 
   std::cout << "The non mutual prime factors the given numbers are: ";
-  primeFactors(n);
-  int previous = 0;
-  while (!primeFactorsInStack.empty()) {
-    if ((n2 % primeFactorsInStack.top() != 0) && (previous != primeFactorsInStack.top())) {
-      std::cout << primeFactorsInStack.top();
-    }
-    previous = primeFactorsInStack.top();
-    primeFactorsInStack.pop();
-
-  }
-  std::cout << "\n";
+  primeFactors(n, n2);
 
   return 0;
 
