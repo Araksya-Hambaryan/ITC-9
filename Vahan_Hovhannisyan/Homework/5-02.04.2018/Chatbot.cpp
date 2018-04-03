1f16744c592bba3bcceadd7f4773e5839e1427ff
unknown <vahan.hovhannisyan.1997@gmail.com>
2018-04-03 10:34:17 +0400

    Adding Chatbot and Clock

---
+++ Vahan_Hovhannisyan/Homework/5-02.04.2018/Chatbot.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream> 
+#include <string>
+
+  int main() {
+    std::string Response[] = {
+      "Hi!",
+      "Very well, and you ?.",
+      "Yeah. It was really exhausting!",
+      "Thanks, you too !.",
+    };
+    std::string Talk[] = {
+      "Hi!",
+      "How do",
+      "Are you done with your homework yet?",
+      "Have a good day!",
+    };
+    std::cout << "Talk to me please: " << std::endl;
+    std::string userTalk;
+
+    do {
+        std::getline (std::cin, userTalk);
+     // std::getline >> userTalk;
+      if (userTalk == Talk[0]) {
+        std::cout << Response[0] << std::endl;
+      }
+      else if (userTalk == Talk[1]) {
+      
+        std::cout << Response[1] << std::endl;
+      }
+      else if (userTalk == Talk[2]) {
+        std::cout << Response[2] << std::endl;
+      }
+      else if (userTalk == Talk[3]) {
+        std::cout << Response[3] << std::endl;
+      }
+      else {
+        std::cout << "Sorry I can't understand you." << std::endl;
+      }
+
+    } while (userTalk != Talk[3]);
+
+    return 0;
+  }
---
+++ Vahan_Hovhannisyan/Homework/5-02.04.2018/Clock.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+  int main() {
+    int Rings = 0;
+    int num1Starting = 0;
+    int num2Starting = 0;
+    char opStarting = 0;
+    std::cout << "Please input the starting point as shown - 3:45 - and press 'Enter' : ";
+    std::cin >> num1Starting >> opStarting >> num2Starting;
+
+    int num1Ending = 0;
+    int num2Ending = 0;
+    char opEnding = 0;
+    std::cout << "Please input the ending point as shown - 3:45 - and press 'Enter' : ";
+    std::cin >> num1Ending >> opEnding >> num2Ending;
+    if ((num1Ending < 0 ||
+        num1Ending > 11 ||
+        opEnding != ':' ||
+        num2Ending < 0 ||
+        num2Ending > 59 ||
+        num1Starting < 0 ||
+        num1Starting > 11 ||
+        opStarting != ':' ||
+        num2Starting < 0 ||
+        num2Starting > 59 ||
+        num1Starting > num1Ending)) {
+
+      std::cout << "Wrong input! \n";
+
+    } else {
+      if (num1Ending - num1Starting > 1) {
+        for (int i = 0; i < num1Ending - num1Starting; i++) {
+          if (num2Starting <= 7 && num2Ending >= 7) {
+            Rings++;
+          }
+          Rings++;
+        }
+      }
+      Rings = Rings + num1Ending - num1Starting;
+
+      std::cout << Rings;
+    }
+    return 0;
+  }
