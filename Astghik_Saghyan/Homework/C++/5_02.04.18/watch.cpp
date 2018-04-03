bfb3403cf92c644b6af4d0d1f053d05bd7da8e08
astghiksaghyan <astghiksaghyan@gmail.com>
2018-04-03 13:09:33 +0400

    Changed the solution

--- Astghik_Saghyan/Homework/C++/5_02.04.18/watch.cpp
+++ Astghik_Saghyan/Homework/C++/5_02.04.18/watch.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
+int calcRings(int sHour, int sMinut, int eHour, int eMinut, int pivot) {
+	int countRings;
+	int duration = eHour * 60 + eMinut - sHour * 60 -sMinut;
+	if(duration < 0) {
+		std::cout << "Enter valid range!" << std::endl;
+		return 0;
+	}
+	if(duration < 60) {
+		return 0;
+	}
+	if(sMinut > pivot) {
+		duration = duration - (60 - sMinut);
+	} else {
+		duration = duration - (pivot - sMinut);
+	}
+	countRings = duration/60 + 1;
+	return countRings;
+}
+
 int main() {
 	int strHour;
 	int strMinut;
 	int endHour;
 	int endMinut;
+	int ringsHour;
+	int ringsMinut;
 	std::cout << "Enter the start hour:  " << std::endl;
 	std::cin >> strHour;
 	std::cout << "Enter the start minut:  " << std::endl;
@@ -13,22 +34,17 @@ int main() {
 	std::cin >> endHour;
 	std::cout << "Enter the end minut:  " << std::endl;
 	std::cin >> endMinut;
-	int strTime = strHour * 60 + strMinut;
-	int endTime = endHour * 60 + endMinut;
-	if(endTime > 720) {
-		std::cout << "The end time have to be earlier 12:00" << std::endl;
+	if ( strHour < 1 || strHour > 12 || endHour < 1 || endHour > 12) {
+		std::cout << "Enter valid hour!";
 		return 0;
 	}
-	if(strTime < 60) {
-		std::cout << "The start time have to be after 01:00" << std::endl;
+	if ( strMinut < 0 || strMinut > 60 || endMinut < 0 || endMinut > 60) {
+		std::cout << "Enter valid minut!";
 		return 0;
 	}
-	int time = endTime - strTime;
-	if(time < 0) {
-		std::cout << "Please, enter valid range!" << std::endl;
-		return 0;
-	}
-	int countRing = time/60+time/67 + 1;
-	std::cout << countRing << std::endl;
+	
+	ringsHour = calcRings(strHour, strMinut, endHour, endMinut, 0); 
+	ringsMinut = calcRings(strHour, strMinut, endHour, endMinut, 6); 
+	std::cout << "Clock will ring " << ringsHour + ringsMinut << " time!" << std::endl;
 	return 0;
 }
