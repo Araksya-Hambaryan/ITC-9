70d4732836e7c72bbad18ed59a4cc2097ff5a605
astghiksaghyan <astghiksaghyan@gmail.com>
2018-04-05 01:32:41 +0400

    Tasks for 04.04

---
+++ Astghik_Saghyan/Homework/C++/7_04.04.18/overloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//int foo(int a, int b, float c) {
+//int foo(int a, int b, int c = 3) {
+//int foo(int a, int b, char c = 'l') {
+///    return 20;
+//}
+
+//int foo(int x, int* y) {
+//int foo(int a, int b, int c) {
+//    return 100;
+//}
+
+int main() {
+    
+    int k = 10;
+    int *m = &k;
+//    std::cout << foo(2, 5, 5) << std::endl;
+//    std::cout << foo(2, m) << std::endl;//100
+//    std::cout << foo(2, 5, 2.5) << std::endl;
+  //    std::cout << foo(2, 5, 15) << std::endl;
+   // std::cout << foo() << std::endl;
+   // std::cout << foo() << std::endl;
+   // std::cout << foo() << std::endl;
+    return 0;
+} 
