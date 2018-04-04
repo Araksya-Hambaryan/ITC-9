0f4d86c9fa9e7cf2d140edc3f6d54ceeda2a089d
home <gevor.antonyan94@gmail.com>
2018-04-04 12:33:07 +0400

    corrected tasks

--- Gevorg_Antonyan/Homework/c++/02.04/Common_simple_dividers.cpp
+++ Gevorg_Antonyan/Homework/c++/02.04/Common_simple_dividers.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 bool prime(int number) {
-    bool k = 1;    
+    bool k = 1;
     for( int i = 2; i <= (number / i); i++) {
         if(0 == number % i) {
             k = 0;
@@ -12,76 +12,29 @@ bool prime(int number) {
 }
 
 int main() {
-    int size1 = 0;
-    int size2 = 0;
-    int num1 = 0;
+    int num = 0;
+    int size = 0;
     int num2 = 0;
-    std::cout << "enter natural number1" << ": " ;
-    std::cin >> num1 ;
+    std::cout << "enter natural number" << "-";
+    std::cin >> num ;
     std::cout << "enter natural number2" << ": " ;
     std::cin >> num2;
-    for (int i = 2; i <= num1 / 2 ; ++i) {
-        if (0 == num1 % i) {
+    std::cout << "prime numbers is :" ;
+    for (int i = 2; i <= num / 2 ; ++i) {          // num / 2  ????
+        if (0 == num % i) {
             if (true == prime(i)) {
-                ++size1;
-            }
-        } 
-    }
-    if (0 == size1) {
-       size1 = 1;   
-    }
-    int primenumbers1[size1];
-    if(1 == size1) {
-        primenumbers1[0] = num1;
-    } else {
-        for (int j = 2; j <= num1 / 2; ++j) {
-            int i = 0;
-            if (0 == num1 % j) {
-                if (true == prime(j)) {
-                    primenumbers1[i] = j;
-                    ++i;
+                if(0 != num2 % i) {
+                    std::cout << i << " ," ;
+                    ++size;
                 }
-            }                    
-        }
-    }
-    for (int i = 2; i <= num2 / 2 ; ++i) {
-        if (0 == num2 % i) {
-            if (true == prime(i)) {
-                 ++size2;
             }
         }
     }
-    if (0 == size2) {
-       size2 = 1;   
-    }
-    int primenumbers2[size2];
-    if(1 == size2) {
-        primenumbers2[0] = num2;  
-    } else {
-            for (int j = 2; j <= num2 / 2; ++j) {
-                int i = 0;
-                if (0 == num2 % j) {
-                    if (true == prime(j)) {
-                        primenumbers2[i] = j;
-                        ++i;
-                    }
-                }    
-            }
-        }
-    std::cout << " prime numbers - " ;
-    for (int i = 0; i < size1; ++i) {
-        bool x = 1;
-        for (int j = 0; j < size2; ++j) {
-            if (primenumbers1[i] == primenumbers2[j]) {
-               x = 0;
-               break;
-            }
-        
-        if (true == x) {
-            std::cout << primenumbers1[i] << ", " ;
-            }
+    if (0 == size) {
+        if (0 != num2 % num) {
+            std::cout << num << std::endl;
         }
     }
-    std::cout<< std::endl;    
+    std::cout<<std::endl;
     return 0;
-}   
+}
--- Gevorg_Antonyan/Homework/c++/02.04/Ring.cpp
+++ Gevorg_Antonyan/Homework/c++/02.04/Ring.cpp
@@ -7,28 +7,42 @@ int main() {
     int ehour = 0;
     int eminut = 0;
     int ring = 0;
+//    bool k = 1;
     std::cout << "enter begin time xx xx" << std::endl;
     std::cin >> bhour >> bminut ;
     std::cin >> ehour >> eminut ;
-    if (ehour == 12 && eminut > 0) {
+//    if ('0' > (char)bhour > '9' || '0' > (char)ehour > '9' || '0' > (char)bminut > '9' || '0' > (char)eminut > '9') {
+//        k = 0;
+//    }
+//    while (0 == k) {
+//        std::cout << "enter correct time xx xx" << std::endl;
+//        std::cin >> bhour >> bminut ;
+//        std::cin >> ehour >> eminut ;
+//        if ('0' > (char)bhour > '9' || '0' > (char)ehour > '9' || '0' > (char)bminut > '9' || '0' > (char)eminut > '9') {
+//            k = 0;
+//        } else {
+//           k = 1;
+//        }
+//    }
+    if (12 == ehour && 0 < eminut) {
         eminut = -1;
     }
-    if (bhour == 12 && (bminut != 0 || 12 != ehour || 0 != eminut)) {
+    if (12 == bhour && (0 != bminut || 12 != ehour || 0 != eminut)) {
         eminut = -1;
     }
-    while (0 > bhour || 0 > bminut || 0 > ehour || 0 > eminut || 12 < bhour || 12 < ehour || 60 < bminut || 60 < eminut) {
+    while (0 > bhour || 0 > bminut || 0 > ehour || 0 > eminut || 12 < bhour || 12 < ehour || 60 < bminut || 60 < eminut || bhour > ehour || (ehour == bhour && bminut > eminut)) {
         std::cout << "enter correct time" << std::endl;
         std::cin >> bhour >> bminut ;
         std::cin >> ehour >> eminut ;
     }
     ring = 2 * (ehour - bhour) ;
-    if (bminut == 0) {
+    if (0 == bminut || 7 == bminut) {
         ring = ring +1;    
     }
-    if (bminut > 7) {
+    if (7 < bminut) {
         ring = ring -1;
     }
-    if (eminut >= 7) {
+    if (7 <= eminut) {
         ring = ring + 1;
     }
     std::cout << "will ring " << ring << " items" << std::endl;
