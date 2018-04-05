6175bcd56cbd6f5275aba71a63d9449246801dc2
Arman <avdalyan1999@gmail.com>
2018-04-05 21:09:16 +0400

    Updated clock task

--- Arman_Avdalyan/Homeworks/C++/5_02.04/clock.cpp
+++ Arman_Avdalyan/Homeworks/C++/5_02.04/clock.cpp
@@ -7,21 +7,13 @@ int getCountOfRings(double hour1, double hour2) {
     double sevenMin = 0.07;
     double epsilon = 0.001;
     for (double i = hour1; i <= hour2; i += 0.01) {
-        //std :: cout << "i = " << i << std :: endl;
         temp = i + epsilon;
-        //std :: cout << "temp = " << temp << std :: endl;
-        //std :: cout << "abs(i - temp) = " << std :: abs(i - temp) << std :: endl;
         if (std :: abs(i - temp) < epsilon) {
-            std :: cout << "Time = 0" << std :: endl;
             ++count;
         }
         if (std :: abs(i - temp - sevenMin) < epsilon){
-            std :: cout << "Time = 0.7" << std :: endl;
             ++count;
-            //std :: cout << "old i = " << i << std :: endl;
-            //i = temp + 0.07;
             i += 1 - sevenMin - 0.01;
-            //std :: cout << "new i = " << i << std :: endl;
         }
     }
     return count;
