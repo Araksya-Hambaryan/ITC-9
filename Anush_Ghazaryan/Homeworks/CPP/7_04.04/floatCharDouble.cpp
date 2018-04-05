7c16a5e7f88cbdf67b0e73fbb8c83f5f67cd64e5
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-04-05 08:42:56 +0400

    Added

---
+++ Anush_Ghazaryan/Homeworks/CPP/7_04.04/floatCharDouble.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+//int  foo( int varI , float varF, char arr [ ] = "Tux" ) {
+		
+		
+//		return  varI, varF;
+//	}
+
+int  foo( int varI, double varD ) {
+
+	
+	
+	return  varD;
+}
+
+int main() {
+
+//	std::cout <<  foo(7, 7 );//error  condidate expects 2 arguments 3 provided
+	// std::cout << foo('a', 7); // warning char = string "Tux"
+
+//	std::cout << foo(8.1, "ab");
+        std::cout << foo(7.1, 7);
+
+return 0;
+}
