58be3c29b77df3c0f42adabb1bfa3a0999d41615
Mikayel Gabrielyan <mikayel93gabrielyan@gmail.com>
2018-04-07 01:55:32 +0400

    Done fibonacci

--- Mikayel_Gabrielyan/Homework/C++/8_06.04.2018/fibonacci.cpp
+++ Mikayel_Gabrielyan/Homework/C++/8_06.04.2018/fibonacci.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 
-int fib(int num) {  
+int fib(int num, unsigned int &count) {
+	count++;
 	if(1 == num || 2 == num) {
 		return 1;
 	} 
-	return fib(num - 1) + fib(num - 2);
+	return fib(num - 1, count) + fib(num - 2, count);
 }
 int main() {
 	int number = 0;
+	unsigned int count = 0;
 	std::cout<<"Mutqagrel Fibonaccii sharqi elementi hamar@ ->";
 	std::cin>>number;
 	if(1 > number) {
@@ -15,7 +17,8 @@ int main() {
 	} else if(46 < number) {
 		std::cout<<"Tiv@ mec e int-i chapic -> \n";
 	} else {
-		std::cout<<"Fibonaccii sharqi "<<number<<"-rd elementn e -> " << fib(number)<<'\n';
+		std::cout<<"Fibonaccii sharqi "<<number<<"-rd elementn e -> " << fib(number, count)<<'\n';
+		std::cout<<"Funkcian kanchvel e "<<count<<" angam \n";
 	}
 return 0;
 }
