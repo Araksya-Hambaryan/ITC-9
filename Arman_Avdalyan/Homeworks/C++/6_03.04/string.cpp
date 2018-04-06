682ba149b4b532cf553d7973ca3f19d17c140f51
Arman <avdalyan1999@gmail.com>
2018-04-06 22:13:38 +0400

    Updated string task

--- Arman_Avdalyan/Homeworks/C++/6_03.04/string.cpp
+++ Arman_Avdalyan/Homeworks/C++/6_03.04/string.cpp
@@ -2,56 +2,48 @@
 
 int inputVariant();
 
-void variant1(std :: string& str, int& i) {
+void variant1(std :: string& str) {
     char ch = ' ';
     int count = 0;
-    i = 0;
     std :: cin >> ch;
-    while (str[i] != '\0') {
-        if (str[i] == ch) {
+    for (int i = 0; str[i] != '\0'; ++i){
+        if(str[i] == ch) {
             ++count;
         }
-        ++i;
     }
     std :: cout << "Count of ' " << ch << " ' symbol = " << count << std :: endl;
 }
 
-void variant2(std :: string& str, int& i, bool& flag) {
+void variant2(std :: string& str, bool& flag) {
     flag = 0;
     char ch1 = ' ';
     char ch2 = ' ';
-    i = 0;
     std :: cin >> ch1 >> ch2;
-    while (str[i] != '\0') {
+    for (int i = 0; str[i] != '\0'; ++i) {
         if (str[i] == ch1) {
             str[i] = ch2;
             flag = 1;
         }
-        ++i;
     }
     if (!flag) {
         std :: cout << "There is not " << ch1 << " symbol to replace.\n\n";
     }
 }
 
-void variant3(std :: string& str, int& i, bool& flag) {
+void variant3(std :: string& str, bool& flag) {
     int j = 0;
     char ch1 = ' ';
-    i = 0;
     flag = 0;
     std :: cin >> ch1;
-    while (str[i] != '\0') {
+    for (int i = 0; str[i] != '\0'; ) {
         if (str[i] == ch1) {
-            j = i;
-            while (str[j + 1] != '\0') {
+            for(j = i; str[j + 1] != '\0'; ++j) {
                 str[j] = str[j + 1];
-                ++j;
             }
             str[j] = ' ';
             flag = 1;
         }
         if (1 != flag) {
-
             ++i;
         }
         flag = 0;
@@ -61,12 +53,11 @@ void variant3(std :: string& str, int& i, bool& flag) {
     }
 }
 
-void make(std :: string str) {
+void make(std :: string& str) {
     int var = 0;
-    int i = 0;
     bool flag = 0;
     char temp = ' ';
-    while (true) {
+    while (var != 4) {
         var = inputVariant();
         if(0 == str.length()) {
             std :: cout << "You remove the whole string. \n";
@@ -74,22 +65,20 @@ void make(std :: string str) {
         }
         switch(var) {
             case 1:
-                variant1(str, i);
+                variant1(str);
                 break;
             case 2:
-                variant2(str, i, flag);
+                variant2(str, flag);
                 break;
             case 3:
-                variant3(str, i, flag);
+                variant3(str, flag);
                 break;
-            case 4:
-                std :: cout << "Good luck.\n\n";
-                return;
             default:
                 std :: cout << "Please input again.\n\n";
         }
         std :: cout << str << std :: endl;
     }
+    std :: cout << "Good luck.\n\n";
 }
 
 void input(std :: string& str) {
@@ -99,10 +88,10 @@ void input(std :: string& str) {
 }
 
 int inputVariant() {
-    int variant = 0;
+    int variant = '0';
     std :: cout << "\n\nChoose one of this actions\n 1 - Return count of elements\n 2 - replace character\n 3 - remove element \n 4 - exit\n\n ";
     std :: cin >> variant;
-    while (!std :: cin || variant < 1 || variant > 4) {
+    if (!std :: cin || variant < 1 || variant > 4) {
         std :: cin.clear();
         std :: cin.ignore();
         std :: cout << "\nPlease choose one of actions : ";
@@ -112,7 +101,7 @@ int inputVariant() {
 }
 
 int main() {
-    std :: string str = " ";
+    std :: string str;
     input(str);
     make(str);
     return 0;
