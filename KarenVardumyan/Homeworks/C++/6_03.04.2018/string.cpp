00d1123821ed083ab59a736975c906e39a7a1da8
karen <karen@k-desktop.(none)>
2018-04-06 04:44:52 +0400

    Update tack-string

--- KarenVardumyan/Homeworks/C++/6_03.04.2018/string.cpp
+++ KarenVardumyan/Homeworks/C++/6_03.04.2018/string.cpp
@@ -1,89 +1,101 @@
 #include<iostream>
+#include<string>
 
-int qanak (char a, char array[]) {
- 
-    int counter = 0;
-    for(int i = 0; 50 > i; ++i) {
-        if(array[i] == a) {
-	    counter++;
+using namespace std;
+
+int qanak (char*,int,char);
+
+void popoxel (char,char,char*,int ); // teksti mej a-n poxarinel b-ov
+
+void jnjel (char,char*,int);         //teksti mejic jnjum e nshvac simvoly- simvolnery 
+
+int main () {
+
+    string array;      
+    cout<<"\nNermuceq voreve tekst \n\n";
+    getline(cin,array);
+    int hraman;
+    int chap = array.length();
+    char *argument = &array[0];
+
+    cout<<"\nNermuceq hramani hamary,sexmeq ENTER heto katareq hamapatasxan gorcoxutyunery\n";
+    cout<<"1. nermuceq tar.Cragiry khashvi te qani angam e tekstum ayd tary krknvum \n";
+    cout<<"2. nermuceq erku tar.Cragiry teksti mej arajiny kpoxarini erkrordov\n";
+    cout<<"3. nermuceq tar.Cragiry ayun kjnji teksti mejic\n";
+    cout<<"4. greq 4 ev durs kgaq cragric\n\n";
+    cin>>hraman;
+
+    while (hraman != 4) {
+	
+        switch ( hraman ) {
+             case 1 : {
+	        cout<<"Nermuceq tar.Cragiry khashvi te qani angam e tekstum ayd tary krknvum \n";
+                char tar;
+	        cin>>tar;
+	        cout<<"teksti mej "<<tar<<" tary krknvum e "<<qanak(argument,chap,tar)<<" angam\n";
+                cout<<"Nermuceq hajord hramani hamary ";
+                break;
+             }
+             case 2 : {
+	         cout<<"Nermuceq erku tar.Cragiry teksti mej arajiny kpoxarini erkrordov\n";
+                 char tar1;
+                 char tar2;
+	         cin>>tar1>>tar2;
+	         popoxel(tar1,tar2,argument,chap);
+                 cout<<"Nermuceq hajord hramani hamary ";
+
+                 break;
+             }
+	     case 3 : {
+	         cout<<"Nermuceq tar.Cragiry ayun kjnji teksti mejic\n";
+                 char tar;
+	         cin>>tar;
+                 jnjel(tar, argument, chap);
+                 cout<<"Nermuceq hajord hramani hamary ";
+	         break;
+             }             
 	}
+        cin>>hraman; 
+    }
+    cout<<"Hajoxutyun!!!\n";
+  
+    return 0;
+}
+
+int qanak (char* array, int chap,char a) {
+
+    int counter = 0;
+    for(int i = 0; chap > i; ++i) {
+        if(array[i] == a) {
+            counter++;
+        }
     }
     return counter;
 }
 
-void popoxel (char a,char b, char array[]) { // teksti mej a-n poxarinel b-ov
+void popoxel (char a,char b, char* array,int chap) { // teksti mej a-n poxarinel b-ov
 
-    for(int i = 0; 50 > i; ++i){
-	if(array[i] == a){
-	    array[i] = b;
-	}
+    for(int i = 0; chap > i; ++i){
+        if(array[i] == a){
+            array[i] = b;
+        }
     }
-    for(int i=0; 50 > i; ++i){
-	std::cout<<array[i];
+    for(int i=0; chap > i; ++i){
+        cout<<array[i];
     }
-    std::cout<<"\n";
+    cout<<"\n";
 }
 
-void jnjel (char a, char array[]) { //teksti mejic jnjum e a- simvolnery 
+void jnjel (char a, char* array, int chap) { //teksti mejic jnjum e a- simvolnery 
 
-    for(int i = 0; 50 > i; ++i){
+    for(int i = 0; chap > i; ++i){
         if(array[i] == a){
             array[i] = ' ';
         }
     }
-    for(int i=0; 50 > i; ++i){
-        std::cout<<array[i];
+    for(int i=0; chap > i; ++i){
+        cout<<array[i];
     }
-    std::cout<<"\n";
+    cout<<"\n";
 }
 
-
-int main () {
-
-    char array[50];
-    std::cout<<"Nermuceq voreve tekst \n";
-    std::cin >> array;
-    std:: cout <<"\n" << array << "\n\n";
-    std::cout<<"Nermuceq hramani hamary,sexmeq ENTER heto katareq hamapatasxan gorcoxutyunery\n";
-    std::cout<<"1. nermuceq tar.Cragiry khashvi te qani angam e tekstum ayd tary krknvum \n";
-    std::cout<<"2. nermuceq erku tar.Cragiry teksti mej arajiny kpoxarini erkrordov\n";
-    std::cout<<"3. nermuceq tar.Cragiry ayun kjnji teksti mejic\n";
-    std::cout<<"4. greq 4 ev durs kgaq cragric\n\n";
-    
-    int hraman;
-    std::cin>>hraman;
-
-    while( hraman != 4){
-	
-        switch ( hraman ) {
-             case 1 : {
-	        std::cout<<"Nermuceq tar.Cragiry khashvi te qani angam e tekstum ayd tary krknvum \n";                    char tar;
-	        std::cin>>tar;
-	        std::cout<<"teksti mej "<<tar<<" tary krknvum e "<<qanak(tar, array)<<" angam\n";
-                std::cout<<"Nermuceq hajord hramani hamary ";
-                break;
-             }
-             case 2 : {
-	         std::cout<<"Nermuceq erku tar.Cragiry teksti mej arajiny kpoxarini erkrordov\n";
-                 char tar1;
-                 char tar2;
-	         std::cin>>tar1>>tar2;
-	         popoxel(tar1,tar2,array);
-                 std::cout<<"Nermuceq hajord hramani hamary ";
-                 break;
-             }
-	     case 3 : {
-	         std::cout<<"Nermuceq tar.Cragiry ayun kjnji teksti mejic\n";
-                 char tar;
-	         std::cin>>tar;
-                 jnjel(tar, array);
-                 std::cout<<"Nermuceq hajord hramani hamary ";
-	         break;
-             }             
-	}
-        std::cin>>hraman; 
-    }
-    std::cout<<"Hajoxutyun!!!\n";
-  
-  return 0;
-}
