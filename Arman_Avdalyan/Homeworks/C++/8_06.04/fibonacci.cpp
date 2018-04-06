d9d0893d2d27672a3e7431521649d2dfbf648ad7
Arman <avdalyan1999@gmail.com>
2018-04-06 22:15:26 +0400

    Added fibonacci numbers

---
+++ Arman_Avdalyan/Homeworks/C++/8_06.04/fibonacci.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int fib(int number, int& count, int first = 0, int second = 1) {
+    count++;
+    if (0 == number) {
+        return second;
+    }
+    fib(number - 1, count, second, first + second);
+}
+
+int input() {
+    int num = 0;
+    std :: cout << "Input number : ";
+    std :: cin >> num;
+    while (!std :: cin || num < 0) {
+        std :: cin.clear();
+        std :: cin.ignore();
+        std :: cout << "Please input correct : ";
+        std :: cin >> num;
+    }
+    return num;
+}
+
+int main() {
+    int number = input();
+    int count = 0;
+//    if (0 == number) {
+//        std :: cout << "Fibonnaci number = 1.\n";
+//    } else {
+        std :: cout << "Fibonacci number = " << fib(number, count) << ".\n";
+//    }
+    std :: cout << "Count = " << count << std :: endl;
+    return 0;
+}
