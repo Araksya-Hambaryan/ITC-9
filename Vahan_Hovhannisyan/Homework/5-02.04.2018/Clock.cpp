2e4f7a5718d277b5f2a69b9b238c52b9645dfa46
unknown <vahan.hovhannisyan.1997@gmail.com>
2018-04-06 13:20:43 +0400

    Improved chatbot,clock,prime factors, primefactors non mutual

--- Vahan_Hovhannisyan/Homework/5-02.04.2018/Clock.cpp
+++ Vahan_Hovhannisyan/Homework/5-02.04.2018/Clock.cpp
@@ -1,50 +1,42 @@
 ﻿#include <iostream>
 
   int main() {
-    int Rings = 0;
-    int num1Starting = 0;
-    int num2Starting = 0;
+    int rings = 0;
+    int startingHours = 0;
+    int startingMinutes = 0;
     char opStarting = 0;
     std::cout << "Please input the starting point as shown - 3:45 - and press 'Enter' : ";
-    std::cin >> num1Starting >> opStarting >> num2Starting;
+    std::cin >> startingHours >> opStarting >> startingMinutes;
 
-    int num1Ending = 0;
-    int num2Ending = 0;
+    int endingHours = 0;
+    int endingMinutes = 0;
     char opEnding = 0;
     std::cout << "Please input the ending point as shown - 3:45 - and press 'Enter' : ";
-    std::cin >> num1Ending >> opEnding >> num2Ending;
-    if ((num1Ending < 0 ||
-        num1Ending > 11 ||
+    std::cin >> endingHours >> opEnding >> endingMinutes;
+    if ((endingHours < 0 ||
+        endingHours > 11 ||
         opEnding != ':' ||
-        num2Ending < 0 ||
-        num2Ending > 59 ||
-        num1Starting < 0 ||
-        num1Starting > 11 ||
+        endingMinutes < 0 ||
+        endingMinutes > 59 ||
+        startingHours < 0 ||
+        startingHours > 11 ||
         opStarting != ':' ||
-        num2Starting < 0 ||
-        num2Starting > 59 ||
-        num1Starting > num1Ending)) {
+        startingMinutes < 0 ||
+        startingMinutes > 59 ||
+        startingHours > endingHours)) {
 
-      std::cout << "Wrong input! \n";
+      std::cout << "\nSorry,wrong input! \n";
 
     } else {
-      if (num1Ending - num1Starting > 1) {
-        for (int i = 0; i < num1Ending - num1Starting; i++) {
-          if (num2Starting <= 7 && num2Ending >= 7) {
-            Rings++;
-          }
-          Rings++;
+      int beginning = startingHours * 60 + startingMinutes;
+      int ending = endingHours * 60 + endingMinutes;
+      for (int i = beginning; i <= ending; i++) {
+        if (0 == i % 60 || 0 == (i - 7) % 60) {
+          rings++;
         }
-      }
-      
-      if (num2Starting==0) {
-          Rings++;
-      }
-      if (num2Starting <= 7 && num2Ending >= 7) {
-            Rings++;
-          }
 
-      std::cout << Rings;
+      }
+      std::cout << "The number of rings is: " << rings;
     }
     return 0;
   }
