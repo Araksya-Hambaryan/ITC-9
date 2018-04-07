d1e9dd37a35c28926a03b52d4d454960bee99be2
home <gevor.antonyan94@gmail.com>
2018-04-07 15:47:40 +0400

    added

--- Gevorg_Antonyan/Homework/c++/04.04/Letter.cpp
+++ Gevorg_Antonyan/Homework/c++/04.04/Letter.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void tar(char** arr, int size, char symbol, char symbol0) {
+void tar(char**& arr, int size, char symbol, char symbol0) {
     arr = new char*[size];
     for(int i = 0; i < size; ++i) {
         arr[i] = new char[size];
@@ -21,6 +21,47 @@ void tar(char** arr, int size, char symbol, char symbol0) {
     }
 }
 
+void replace (char**& text, int size) {
+    char change;
+    char str;
+    std::cout << "pleace enter change letter" << std::endl;
+    std::cin >> change;
+    std::cout << "pleace enter new letter" << std::endl;
+    std::cin >> str;
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {            
+            if(text[i][j] == change) {
+                text[i][j] = str;
+            }
+        }
+    }
+}
+
+int count (char** text, int size) {
+    char str;
+    std::cout << "enter quantity symbol" <<std::endl;
+    std::cin >> str;
+    int quantity = 0;
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {
+            if (text[i][j] == str) {
+                ++quantity;
+            }
+        }
+    }
+    return quantity;
+}
+
+
+void print(char** arr, int size) {
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j){
+           std::cout << arr[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main() {
     int size = 0;
     char symbol0; 
@@ -31,17 +72,11 @@ int main() {
     std::cin >> symbol0;
     std::cout << "enter main symbol" <<std::endl;
     std::cin >> symbol;
-//    char* arr;
-//    for(int i = 0; i < size; ++i) {
-//        char arr[i] = arr[size];
-//    }
-
-    tar(&arr, size, symbol, symbol0);
-    for(int i = 0; i < size; ++i) {
-        for(int j = 0; j < size; ++j){
-           std::cout << arr[i][j];
-        }
-        std::cout << std::endl;
-    }
+    char** arr;
+    tar(arr, size, symbol, symbol0);
+    print(arr, size);
+    replace(arr, size);
+    print(arr, size);    
+    std::cout << count(arr, size) << std::endl;
     return 0;
 }
