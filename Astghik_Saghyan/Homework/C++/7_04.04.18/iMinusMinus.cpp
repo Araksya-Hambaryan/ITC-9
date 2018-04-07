ccbce8ff5a33d18579fb73797ca31148fe5696cb
astghiksaghyan <astghiksaghyan@gmail.com>
2018-04-07 22:52:52 +0400

    1.Maked dinamic size of letter; 2.the number for pre and post enter user

--- Astghik_Saghyan/Homework/C++/7_04.04.18/iMinusMinus.cpp
+++ Astghik_Saghyan/Homework/C++/7_04.04.18/iMinusMinus.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 
 int preDecrease(int &i) {
+    std::cout << "Enter a number:\t";
+    std::cin >> i;
 	i = i - 1;
 	return i;
 }
 
 int postDecrease(int &i) {
+    std::cout << "Enter a number:\t";
+    std::cin >> i;
 	int j = i;
 	i = i - 1;
 	return j;
 }
 
 int main() {
-	int k = 10;
-	std::cout << preDecrease(k) << std::endl;
-    std::cout << k << std::endl;
-	std::cout << postDecrease(k) << std::endl;
-	std::cout << k << std::endl;
+	int k;
+	std::cout << "Call of PreDec:\t" << preDecrease(k) << std::endl;
+    std::cout << "Number:\t" << k << std::endl;
+	std::cout << "Call of postDec:\t" << postDecrease(k) << std::endl;
+	std::cout << "Number:\t" << k << std::endl;
 	return 0;
 }
--- Astghik_Saghyan/Homework/C++/7_04.04.18/letter.cpp
+++ Astghik_Saghyan/Homework/C++/7_04.04.18/letter.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 
-void drowLetter( char**& matrix, char symbol) {
-    matrix = new char*[5];
-    for(int i = 0; i < 5; i++) {
-        matrix[i] = new char[3];
+void drowLetter( char**& matrix, char symbol, int& size) {
+    std::cout << "Enter the size (from 6 to 60):\t";
+    std::cin >> size;
+    std::cout << "Size:\t" << size << std::endl;
+    matrix = new char*[size];
+    while(6 > size || 60 < size) {
+        std::cout << "Please, enter a number from the range!" << std::endl;
+        std::cin >> size;
     }
-    for(int i = 1; i < 5; i++) {
-        for(int j = 0; j < 3; j++) {
-            if(0 == j || 2 == j) {
+
+    for(int i = 0; i < size; i++) {
+        matrix[i] = new char[size/3*2];
+    }
+    for(int i = 1; i < size; i++) {
+        for(int j = 0; j < size/3*2; j++) {
+            if(size/5+1 > i || size/2 == i || size/5 > j || (size/3*2)-size/5-1 < j) {
                 matrix[i][j] = symbol;
             } else {
                 matrix[i][j] = '.';    
             }
         }    
     }
-    matrix[0][1] = symbol;
-    matrix[2][1] = symbol;
-    matrix[0][0] = '.';
-    matrix[0][2] = '.';
 }
 
 
 int main() {
     char** m;
-    drowLetter( m, 'a');
-    for (int i = 0; i < 5; i++) {
-        for(int j = 0; j < 5; j++) {
+    int size;
+    drowLetter( m, 'a', size);
+    std::cout << "size after func:\t" << size << std::endl;
+    for (int i = 0; i < size; i++) {
+        for(int j = 0; j < size/3*2; j++) {
             std::cout << m[i][j] << " ";
         }
         std::cout << std::endl;
