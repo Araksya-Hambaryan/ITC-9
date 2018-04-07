8945094ede7393bbb9dafd7878f17d6754646cd9
home <gevor.antonyan94@gmail.com>
2018-04-07 15:42:39 +0400

    corrected

--- Gevorg_Antonyan/Homework/c++/02.04/Interview.cpp
+++ Gevorg_Antonyan/Homework/c++/02.04/Interview.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include <string>
 
+bool question(std::string text, std::string name, bool k) {
+    int ask = 0;
+    k = 1;
+    std::string answer[5] = {"barev", "vonc es?", "gorcert vonc en?", "dasert vonc en?", "ctesutyun"};
+    for(int i = 0; i < 5; ++i) {
+        if(text == answer[i]) {
+            ask = i;
+            break;
+        } else {
+            ask = 5;
+        }
+    }
+    switch(ask) {
+        case(0):
+            std::cout << "bot:barev " << name << std::endl;
+            break;
+        case(1):
+            std::cout << "bot:lav em , shnorhakalutyun!  " << std::endl;
+            break;
+        case(2):
+            std::cout << "bot:der shxatum em)) " << std::endl;
+            break;
+        case(3):
+            std::cout << "bot:lav en, arajadimum em)"  << std::endl;
+            break;
+        case(4):
+            std::cout << "bot:ctesutyun  " << name << std::endl;
+            k = 0;
+            break;
+        case(5): 
+            std::cout << "bot:kneres chhaskaca, xndrum em noric harcru" << name << std::endl;
+    }
+    return k;
+}
+
+
+
 
 int main() {
     bool k = 1;
-    int ask = 0;
     std::cout << "bot:Inch e qo anune?  " << std::endl;
     std::string name;
     std::getline(std::cin , name) ;
@@ -15,23 +51,10 @@ int main() {
     std::cout << "3.gorcert vonc en?" << std::endl;
     std::cout << "4.dasert vonc en?" << std::endl;
     std::cout << "5.ctesutyun (durs galu hamar):" << std::endl; 
-    while (1 == k) {
+    while (k) {
         std::string text;
         std::getline(std::cin ,text);
-        if  ("barev" == text) {
-            std::cout << "bot:barev " << name << std::endl;
-        } else if ("vonc es?" == text) { 
-            std::cout << "bot:lav em , shnorhakalutyun!  " << std::endl;
-        } else if ("gorcert vonc en?" == text) {
-            std::cout << "bot:der shxatum em)) " << std::endl;
-        } else if ("dasert vonc en?" == text) {
-            std::cout << "bot:lav en, arajadimum em)"  << std::endl;
-        } else if ("ctesutyun" == text) {
-            std::cout << "bot:ctesutyun  " << name << std::endl;
-            k = 0;
-        } else {
-            std::cout << "bot:kneres chhaskaca, xndrum em noric harcru" << name << std::endl;
-        }
+        k = question(text, name, k);
     }
     return 0;
 }
--- Gevorg_Antonyan/Homework/c++/02.04/Ring.cpp
+++ Gevorg_Antonyan/Homework/c++/02.04/Ring.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
+#include <cmath>
 
+int convert(std::string num) {
+    int a = 0;
+    for(int i = 0; i < num.size(); ++i) {
+        a = a + ((int)num[i] - 48) * pow(10, num.size() - 1 - i);
+    }
+    return a;
+}
 
 int main() {
-    int bhour = 0;
-    int bminut = 0;
-    int ehour = 0;
-    int eminut = 0;
-    int ring = 0;
     std::cout << "enter begin time xx xx" << std::endl;
-    std::cin >> bhour >> bminut ;
-    std::cin >> ehour >> eminut ;
-    while (0 > bhour || 0 > bminut || 0 > ehour || 0 > eminut || 12 < bhour || 12 < ehour || 60 < bminut || 60 < eminut || bhour > ehour || (ehour == bhour && bminut > eminut) /*|| ('0' > (char)bhour && '9' < (char)bhour ) || ('0' > (char)ehour && '9' < (char)ehour) || ('0' > (char)bminut && '9' < (char)bminut) || ('0' > (char)eminut && '9' < (char)eminut)*/) {
-        std::cout << "enter correct time" << std::endl;
-        std::cin >> bhour >> bminut ;
-        std::cin >> ehour >> eminut ;
-    } 
+    std::string bhour0;
+    std::cin >> bhour0;
+    std::string bminut0;
+    std::cin >> bminut0;
+    std::string ehour0;
+    std::cin >> ehour0;
+    std::string eminut0;
+    std::cin >> eminut0;
+    int bhour = convert(bhour0);
+    int bminut = convert(bminut0);
+    int ehour = convert(ehour0);
+    int eminut = convert(eminut0);
+    int ring = 0;
+    while (0 > bhour || 0 > bminut || 0 > ehour || 0 > eminut || 12 < bhour || 12 < ehour || 60 < bminut || 60 < eminut || bhour > ehour || (ehour == bhour && bminut > eminut)) {
+        std::cout << "enter correct time '00' '00'" << std::endl;
+        std::cin >> bhour0;
+        std::cin >> bminut0;
+        std::cin >> ehour0;
+        std::cin >> eminut0;
+        bhour = convert(bhour0);
+        bminut = convert(bminut0);
+        ehour = convert(ehour0);
+        eminut = convert(eminut0);
+    }
     if (12 == ehour && 0 < eminut) {
         eminut = -1;
     }
     if (12 == bhour && (0 != bminut || 12 != ehour || 0 != eminut)) {
         eminut = -1;
     }
-
     ring = 2 * (ehour - bhour) ;
     if (0 == bminut || 7 == bminut) {
         ring = ring +1;    
