ec0386858deae009dce5cbb8c587309dbf9c7eca
mmelqonyan <mmelqonyan1990@gmail.com>
2018-04-08 15:47:45 +0400

    interwiew with reminders

---
+++ Mher_Melqonyan/Homeworks/C++/updates/chatbot.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string harc;
+    std:: cout << "ChatBoti het shpvelu hamar karox eq grel hetevyal harcer@\n";
+    std::string ask[4] = {"Barev", "Inch ka?", "Vonc es?", "Hajox"};
+    std::string patasxan;
+    for(int i = 0; i < 4; ++i) {
+         std::cout << i+1 << ". " << ask[i] << std::endl;
+        
+    }
+    std::cout << "greq text " << std::endl;
+    while("Hajox" != patasxan) {
+        int ind =4;
+        std::getline (std::cin,harc);
+        for(int i = 0; i < 4; ++i) {
+            if(!ask[i].compare(harc)) {
+                ind = i;
+            }
+        }
+        switch (ind) {
+            case (0):
+                 std::cout << "Barev" << std::endl;
+                 patasxan = "Barev";
+                break;
+            case (1):
+                  std::cout << "Sax lava" << std::endl;
+                  patasxan = "Sax lava";
+                break;
+            case (2):
+                  std::cout << "Orva het" << std::endl;
+                  patasxan = "Orva het";
+                break;
+            case (3):
+                  std::cout << "Hajox" << std::endl;
+                  patasxan = "Hajox";
+                break;
+            default:
+                  std::cout << "Chem haskanum" <<std::endl;
+                  patasxan = "Chem haskanum";
+                break;
+        }
+    }    
+    return 0;
+}
