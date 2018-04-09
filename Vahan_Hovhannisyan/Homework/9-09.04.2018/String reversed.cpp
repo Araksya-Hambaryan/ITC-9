05ddaf2013246a2e23925c819a9a3b92237e6b52
unknown <vahan.hovhannisyan.1997@gmail.com>
2018-04-09 22:53:04 +0400

    Added string reversed

---
+++ Vahan_Hovhannisyan/Homework/9-09.04.2018/String reversed.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+inline void swap(char * a, char * b) {
+  char tmp = * a;
+  * a = * b;
+  * b = tmp;
+}
+
+inline void reverse_string(char * start, char * end) {
+  while (start < end) {
+    swap(start++, end--);
+  }
+
+}
+
+int main() {
+  char str[] = "12381";
+  char * start = std::begin(str);
+  char * end = std::end(str);
+  end -= 2; // end points 1 past the null character, so have to go back 2
+  std::cout << "The original string " << str << ".\n";
+  reverse_string(start, end);
+  std::cout << "The reversed string " << str << ".\n";
+  return 0;
+}
