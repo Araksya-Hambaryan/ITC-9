1b83bba79bf72983432748b7c8edf292aea25aa9
Mikayel Gabrielyan <mikayel93gabrielyan>
2018-04-09 23:11:45 +0400

    Done Sorting

---
+++ Mikayel_Gabrielyan/Homework/C++/9_09.04.2018/sort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int comp(int a, int b) {
+    if (a > b) {
+        return a;	
+    }
+    return b;
+}
+
+void mySort(int* arr, int n, int(*comp)(int, int), char simb) {
+    int value = 0;
+    if ('+' == simb) {
+        value = 0;
+    } else {
+        value = 1;
+    }
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n-1; ++j) {
+            if(comp(arr[j], arr[j+1]) == arr[j + value])  {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+
+}
+
+void print(int* arr, int n) {
+    for(int i = 0; i < n; ++i) {
+        std::cout << arr[i] << "\t";        
+    }
+    std::cout << std::endl;
+}
+
+void creatArray(int* arr, int n) {
+    std::cout << "Mutqagrel zangvaci elementner@ -> ";
+    for(int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }    
+}
+
+int main() {
+    int size = 0;
+    std::cout << "Mutqagrel zangvaci chap@ -> ";
+    std::cin >> size;
+    int arr[size];
+    creatArray(arr, size);
+    char simbol = ' ';
+        std::cout << "Achman kargov dasavorelu hamar mutqagrel + , isk nvazman hamaar -  -> ";
+        std::cin >> simbol;
+    mySort(arr, size, comp, simbol);
+    print(arr, size);
+    return 0;
+}
