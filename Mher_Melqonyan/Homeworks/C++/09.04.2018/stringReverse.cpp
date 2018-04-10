c2eda634d494d1350778ee138fecebdd1288317f
mmelqonyan <mmelqonyan1990@gmail.com>
2018-04-10 14:17:33 +0400

    updated

--- Mher_Melqonyan/Homeworks/C++/09.04.2018/stringReverse.cpp
+++ Mher_Melqonyan/Homeworks/C++/09.04.2018/stringReverse.cpp
@@ -1,14 +1,16 @@
-#include < iostream > 
-#include < string >
+#include <iostream> 
+#include <string>
 
   int main() {
     std::string str;
     std::cout << "nermuceq text\n";
     getline(std::cin, str);
+    char* begin;
+    char* eNd;
     int size = str.size();
-    for (int i = 0; i < 3; ++i) {
-      char * begin = & str.at(0);
-      char * eNd = & str.at(size - 1);
+    for (int i = 0; i < size / 2; ++i) {
+      begin = & str.at(0);
+      eNd = & str.at(size - 1);
       char res;
       res = * (begin + i);
       * (begin + i) = * (eNd - i);
