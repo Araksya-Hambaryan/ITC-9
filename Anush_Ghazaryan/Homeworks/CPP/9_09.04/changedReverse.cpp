a2bee6d3aa1f2f22f7133f98f187ee37ccbd3c46
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-04-12 01:55:28 +0400

    changed

--- Anush_Ghazaryan/Homeworks/CPP/9_09.04/changedReverse.cpp
+++ Anush_Ghazaryan/Homeworks/CPP/9_09.04/changedReverse.cpp
@@ -2,16 +2,16 @@
 #include <string>
 
 int main() {
-	char* front;
-	char* back;	
-    std::string word = "abcdefg";
+    char* front = nullptr;     // declaration of pointers
+    char* back = nullptr;	
+    std::string word = "abcdefg";            //string declaration
     for(int i = 0; i < word.size() / 2; ++i) {
-        front = &word[i];
+        front = &word[i];                   // reversing the  elements of the  string with pointers
         back = &word[word.size() - 1 - i];
         char tmp = *front;
         *front = *back;
         *back = tmp;
     }  
-    std::cout << word  <<std::endl;
+    std::cout << word  <<std::endl;      // printing the reversed string
     return 0;
 }
