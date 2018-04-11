d325145d74d2b143e0ee7cc35d4482877f80ee03
AnahitKaryan <1anahit1@mai.ru>
2018-04-12 00:15:30 +0400

    sorting update

--- Anahit_Karyan/Homeworks/C++/9-09.04.2018/sorting.cpp
+++ Anahit_Karyan/Homeworks/C++/9-09.04.2018/sorting.cpp
@@ -1,47 +1,61 @@
 #include <iostream>
 
-int* sort(int* a,int size,int(*fun)(int ,int )){
+void sort(int* a,int n,int(*fun)(int ,int )){
 
-	int y = 0 ;
-
-	for (int i = 0 ; i < size - 1 ; ++i)
-	{
-		for (int j = 0 ; j < size- i - 1 ; ++j)
-		{
-			if (a[j] > a[j + 1]) {
-
-				y = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = y;
+	for(int i = 0; i < n; ++i) {
+		for(int j = 0; j < n-1; ++j) {
+			if(fun(a[j], a[j+1]) == a[j])  {
+				int temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
 			}
 		}
 	}
 
-
-	for( int i = 0 ; i < size ; ++i)
-	{
-		std::cout << a[i] << " ";
-	}
-
 }
 int poqr (int a,int b){
-	int c = 0;
-	a < b ? c = a : c = b;
-	return c;
+
+	if( a < b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+int mec (int a,int b){
+
+	if( a > b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+
+
+
+void print(int* a,int n){
+	for(int i = 0; i < n;++i){
+		std::cout << a[i] << " ";
+	}
 }
 int main(){
-	int n;
-	std::cout << "Mutqagreq zangvaci chap";
-	std::cin >> n;
-	int arr[n];
-	std::cout << "Mutqagreq zangvac";
-	for (int j = 0 ; j < n ; ++j){
-	
+
+	const int size = 5;
+	int arr[size];
+	std::cout << "Mutqagreq 5 chapani zangvac" << std::endl;
+	for (int j = 0 ; j < size ; ++j){
+
 		std::cin >> arr[j] ;
 
 	}
 
-
-	std::cout << sort(arr,n,poqr);
+	sort(arr,size,poqr);
+	std::cout << "Nvazman kargov dasavorvac" << std::endl;
+	print(arr,size);
+	sort(arr,size,mec);
+	std::cout << std::endl;
+	std::cout << "Achman kargov dasavorvac" << std::endl;
+	print(arr,size);
 	return 0;
 }
+
