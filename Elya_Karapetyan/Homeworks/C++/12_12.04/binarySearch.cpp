9a10bd1fe129d8a9e014979a00c8fd41fce00f00
Elya Karapetyan <elya.karapetyan95@mail.ru>
2018-04-12 23:09:01 +0400

    Update task

--- Elya_Karapetyan/Homeworks/C++/12_12.04/binarySearch.cpp
+++ Elya_Karapetyan/Homeworks/C++/12_12.04/binarySearch.cpp
@@ -13,20 +13,7 @@ int binarySearch(int* array, int left, int rigth, int number) {
     }
     return -1;
 }
-/*
-int findPosition(int* array, int number) {
-    std::cout << "findPos\n";
-    int low = 0;
-    int higth = 1;
-    int value = array[0];
-    while (value < number) {
-        low = higth;
-        higth = 2 * higth;
-        value = array[higth];
-    }
-    return binarySearch(array, low, higth, number);
-}
-*/
+
 void sortingArray(int* array, int size) {
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size - i - 1; ++j) {
@@ -39,14 +26,26 @@ void sortingArray(int* array, int size) {
     }
 }
 
+void validNumber(int& number) {
+    while (std::cin.fail() || 1 > number) {    
+        std::cout << "Invalid Value: Try again!" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+        std::cout << "Please enter the intager number: ";
+        std::cin>> number;
+    }
+}
+
 int main() {
     int size = 0;
     std::cout << "Enter size of array: ";
     std::cin >> size;
+    validNumber(size);
     std::cout << "Enter elemets: ";
     int* array = new int[size];
     for (int i = 0; i < size; ++i) {
         std::cin >> array[i];
+        validNumber(array[i]);
     }
     sortingArray(array, size);
     std::cout << "======Array=====" << std::endl;
@@ -57,13 +56,13 @@ int main() {
     int searchedNumber = 0;
     std::cout << "Enter searched number: ";
     std::cin >> searchedNumber;    
-  //  int position = findPosition(array, searchedNumber);
-
-   // std::cout << "pos " << position << std::endl;
-    if (-1 == binarySearch(array, 0, size, searchedNumber)) {
+    validNumber(searchedNumber);
+    int position = binarySearch(array, 0, size -1, searchedNumber);
+    std::cout << std::endl;
+    if (-1 == position) {
         std::cout << searchedNumber << " is not in the array!" << std::endl;
     } else {
-        std::cout << searchedNumber << " is int the array!" << std::endl;
+        std::cout << searchedNumber << " found at index " << position << std::endl;
     }
     return 0;
 }
