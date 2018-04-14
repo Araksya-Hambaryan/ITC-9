60aceb3740f3062ac2ff8fb759dafd04073baa54
karen <karen@k-desktop.(none)>
2018-04-15 00:13:15 +0400

    tack vector is done

---
+++ KarenVardumyan/Homeworks/C++/13_13.04.2018/vector.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+void remuve ( std::vector<char>& );
+
+int main () {
+    std::vector<char>myVector;
+    std::string text;
+    std::cout<<"\nNermuceq tekst.Cragiry kjnji teksti mej gtnvox tvern u simvolnery (baci bacatic).\n\n";
+    std::getline(std::cin,text);
+    for(int i = 0; text.size() > i; ++i){
+	myVector.push_back(text[i]);
+    }
+    remuve(myVector);
+    std::cout<<"\n";
+    for(int i = 0; myVector.size() > i; ++i) {
+	std::cout<<myVector[i];
+    }
+    std::cout<<"\n";
+    return 0;
+}
+
+void remuve (std::vector<char>& myVector){
+    for(int i = 0; i < myVector.size(); i++){
+    	if(('A' > myVector[i] || 'z' < myVector[i] || ('Z' < myVector[i] && 'a' > myVector[i])) 
+            && myVector[i] != ' '){
+	    myVector.erase (myVector.begin()+i);
+            --i;
+	}
+    }
+}
