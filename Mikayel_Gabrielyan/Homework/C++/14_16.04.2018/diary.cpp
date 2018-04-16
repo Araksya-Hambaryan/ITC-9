652d47e014ae96eec97ef99e6064cc4937cccab2
Mikayel Gabrielyan <mikayel93gabrielyan>
2018-04-17 02:32:47 +0400

    Chaged Diary

--- Mikayel_Gabrielyan/Homework/C++/14_16.04.2018/diary.cpp
+++ Mikayel_Gabrielyan/Homework/C++/14_16.04.2018/diary.cpp
@@ -2,32 +2,58 @@
 #include <fstream>
 #include <string>
 
-bool checkName(std::string& inStr) {
-    for(int i = 1; i < sizeof(inStr) -1; ++i)
-        if('A' > inStr[0] || 'Z' < inStr[0] || 'a' > inStr[i] || 'z' < inStr[i]) {
+bool checkName(std::string str) {
+    for(int i = 1; i < sizeof(str) -1; ++i)
+        if('A' > str[0] || 'Z' < str[0] || 'a' > str[i] || 'z' < str[i]) {
             return false;
         } else {
             return true;
         }
 }
 
-int main() {
-    std::string inStr = "";
-    std::string str = "";
+bool checkDate(std::string str){
+    int x = 48;  // Ascii axyusaki 0-i arjeq@
+    int day = (str[0] - x) * 10 + str[1] - x;
+    int month = (str[3] - x) * 10 + str[4] - x;
+    int time = (str[11] - x) * 10 + str[12] - x;
+    int minut = (str[14] - x) * 10 + str[15] - x;
+    int year = 0;
+    int j = 1000;
+    for(int i = 6; i < 10; ++i) {
+        year = year + (str[i] - x) * j;
+        j = j / 10;
+    }
+    if(0 < day && 31 > day && 0 < month && 13 > month && 0 < year && 2019 > year && 0 <= time && 23 >= time && 0 <= minut && 60 >= minut) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void getName(std::string str) {
     std::ifstream file;
-    std::cout << "Mutqagrel anun@ -> ";
-    std::getline(std::cin,inStr);
-    if(checkName(inStr)) {
-        file.open("./file.txt");
-        while(file >> str) {
-            if(str == inStr) {
-                std::cout << str << " ";
-                file >> str;
-                std::cout << str << std::endl;
+    std::string nameStr = "";    
+    std::string dataStr = "";    
+    file.open("file.txt");
+    if(file.is_open()) {
+        while(file >> nameStr >> dataStr) {
+            if(str == nameStr || str == dataStr) {
+                std::cout << nameStr << " " << dataStr << std::endl;
             }
         }
+    }
+}
+
+int main() {
+    std::string str = "";
+    std::cout << "Mutqagrel anun@(orinak Mher) kam amsativ@ (orinak 16.04.2018/15:00) -> ";
+    std::getline(std::cin,str);
+    if(checkName(str)) {
+        getName(str);
+    } else if(checkDate(str)) {
+        getName(str);
     } else {
-        std::cout << "Anvan sxal mutqagrum \n";
+        std::cout << "Sxal Mutqagrum \n";
     }
     return 0;
 }
