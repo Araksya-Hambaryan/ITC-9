63c31aee840533d746ac9a8c0e98234fd3b4c0c4
Nelli <nelli.melkonyan.97@mail.ru>
2018-04-19 06:32:08 +0400

    vector

--- Meliq_Melqonyan/Homeworks/c++/15.17.04.18/vector.cpp
+++ Meliq_Melqonyan/Homeworks/c++/15.17.04.18/vector.cpp
@@ -2,72 +2,92 @@
 
 class Vector {
     private:
-        int size;
+        int erk;
         int* arr;
         int tamp ;
         int count = 0;
         int i = 0;
-    public:
-        Vector(int n, int a) {
-            arr = new int [n];
-            for(int i = 0; i < n; ++i) {
-                arr[i] = a;
-            }
-            size = n;
-            tamp = a;
-        }
-    //mer vector@ krknorinakuma
-        Vector(const Vector& obj) {
-            size = obj.size;
-            tamp = obj.tamp;
-            arr = obj.arr;
-        }
-    //move ka
-        Vector(Vector&& obj) {
-            std :: cout << "move-ka" << std :: endl;
-        }
-
-        int push(int x) {
-            if(i < size) {
-                arr[i] = x;
-                ++count;
-            } else {
-                int newsize = 2 * size;
-                int* array = new int [newsize];
-                for(int j = 0; j < size; ++j) {
+        int* newArr(int size, int& er) {
+            int* array;
+            if(size == 1) {
+                array = new int [er * 2];
+                for(int j = 0; j < i; ++j) {
                     array[j] = arr[j];
                 }
-                for(int j = size; j < newsize; ++j) {
+                for(int j = i; j < i * 2; ++j) {
                     array[j] = tamp;
                 }
-                delete arr;
-                size = newsize;
-                arr = array; 
-                arr[i] = x;
-                ++count;
+                er = 2 * er;
+            } else {
+                array = new int [er - 1];
+                for(int j = 0; j < er - 1; ++j) {
+                    array[j] = arr[j];
+                }
+                er = er - 1;
             }
-            ++i;
-        } 
+            delete arr;
+            return array;
+        }
 
+    public:
+        Vector() {
+            erk = 10;
+            tamp = 0;
+            for(int j = 0; j < erk; ++j) {
+                arr[i] = tamp;
+            }
+        }
+        Vector(int n, int a) {
+            arr = new int [n];
+            for(int j = 0; j < n; ++j) {
+                arr[j] = a;
+            }
+            erk = n;
+            tamp = a;
+        }
+            Vector(const Vector& obj) {
+            erk = obj.erk;
+            i = obj.i;
+            arr = obj.arr;
+            for(int j = 0; j < erk; ++j) {
+                arr[j] = obj.arr[j];
+            }
+            }
+
+            Vector(Vector&& obj) {
+                erk = obj.erk;
+                i = obj.i;
+                arr = obj.arr;
+                obj.arr = nullptr;
+            }
+            ~Vector() {
+                delete []arr;
+                arr = nullptr;
+            }
+            int push(int num) {
+                if(i < erk) {
+                    arr[i] = num;
+                } else {
+                    arr = newArr(1, erk);
+                    arr[i] = num;
+                }
+                ++i;
+            }
+            void pop() {
+                --i;
+                arr[i] = tamp;
+                arr = newArr(0, erk);
+            }
         int get(int index) {
             return arr[index];
         }
 
         void print() {
-            for(int j = 0; j < size; ++j) {
+            for(int j = 0; j < erk; ++j) {
                 std :: cout << arr[j] << " ";
             }
         }   
-
-        void jnjel() {
-            delete arr;
-        }
-
-        void pop() {
-            arr[i] = tamp;
-        }
 };
-
 int main() {
     int size, num;
     std :: cin >> size >> num;
