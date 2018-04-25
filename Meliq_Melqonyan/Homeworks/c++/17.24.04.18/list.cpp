0ede3d6546e4f1a78e2b143d7aa43025172eea77
Meliq Melqonyan <meliqmelqonyan2000@mail.ru>
2018-04-25 21:05:54 +0400

    list

--- Meliq_Melqonyan/Homeworks/c++/17.24.04.18/list.cpp
+++ Meliq_Melqonyan/Homeworks/c++/17.24.04.18/list.cpp
@@ -6,13 +6,17 @@ struct Node {
     Node* last;
     int value;
     Node* next;
+    Node() {
+        next = NULL;
+    }
 };
 
 class List {
     private:
         int count = 0;
+        Node* root = NULL;
     public:
-        Node*  push(int num, Node* root){
+        void  push(int num){
             Node* newNode = new Node;
             Node* temp = root;
             newNode -> value = num;
@@ -26,56 +30,91 @@ class List {
                     temp = temp -> next;
                 }
                 temp -> next = newNode;
+                newNode -> last = temp;
             }
             ++count;
-            return root;
         }
-        void print(Node* root) {
+        void print() {
             Node* temp = root;
-            while(temp != NULL) {
+            while(temp  != NULL) {
                 std :: cout << temp -> value << "  ";
                 temp = temp -> next;
             }
             std :: cout << std :: endl;
 
         }
-        void pop(Node* root) {
+        void pop() {
             Node* temp = root;
             while(temp -> next != NULL) {
                 temp = temp -> next;
-                std::cout << temp -> value << "    "; 
             }
-            root = temp -> last;
-            root -> next = NULL;
+            Node* tem = temp -> last;
+            tem -> next = NULL;
             delete temp;
+            temp = NULL;
+            --count;
+        }
+        void insert(int index, int num) {
+
+            std::cout<<"aaa";
+            if(index < 0 || index > count) {
+                return;
+            }
+            std::cout<<"aaa";
+            Node* temp = root;
+            while(index > 1) {
+                temp = temp -> next;
+                --index;
+            }
+            Node* newNode = new Node;
+            std::cout<<"aaa";
+            newNode -> value = num;
+            newNode -> next = temp -> next;
+            newNode -> last = temp;
+            temp -> next = newNode;
+            temp = temp -> next;
+            temp -> last = newNode;
+            ++count;
+        }
+        void del(int index) {
+            if(index < 0 || index > count) {
+                return;
+            }
+            Node* temp = root;
+            while(index > 1) {
+                temp = temp -> next;
+                --index;
+            }
+            (temp ->last) -> next = temp -> next;
+            (temp -> next) -> last = temp -> last;
+            delete temp;
+            temp = NULL; 
+            --count;
+        }
+        ~List() {
+            Node* temp = root;
+            while (root -> next != NULL) {
+                temp = root;
+                root = root -> next;
+                delete temp;
+                temp = NULL;
+            }
         }
 };
-void print(Node* root) {
-    Node* temp = root;
-    while(temp  != NULL) {
-        std :: cout << temp -> value << "  ";
-        temp = temp -> next;
-    }
-    std :: cout << std :: endl;
-
-}
-
 int main() {
-    Node* root = NULL;
-    std::cout<<" fadfsaf " ;
     List n;
-    int a = 10;
     int b = 20;
-    std::cout <<"     fsdf" ;
-    root = n.push(a,root);
-    root = n.push(100,root);
-    root =n.push(b,root);
-    root =n.push(b,root);
-    root =n.push(b,root);
-    print(root);
-    n.pop(root);
-    n.pop(root);
-    print(root);
+    n.push(50);
+    n.push(100);
+    n.push(b);
+    n.push(b);
+    n.print();
+    n.insert(2,5);
+    n.print();
+    n.del(2);
+    n.print();
+    n.pop();
+    n.print();
     return 0;
 }
 
