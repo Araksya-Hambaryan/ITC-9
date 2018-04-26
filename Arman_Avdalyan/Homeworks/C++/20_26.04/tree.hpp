b83ff8c4c2da774187ebc8572884e99f9e8adc80
Arman <avdalyan1999@gmail.com>
2018-04-27 01:02:45 +0400

    Added homework for 27.04

---
+++ Arman_Avdalyan/Homeworks/C++/20_26.04/tree.hpp
@@ -0,0 +1,36 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+// tree.cpp
+class Tree {
+    struct Node;
+    public:
+        Tree();
+        Tree(const Tree& orig);
+        Tree(Tree&& orig);
+        ~Tree();
+        void remove(int);
+        void add(int);
+        bool find(int);
+        bool isEmpty();
+        int height();
+        void printTree();
+    private:
+        struct Node {
+            int value;
+            Node* left;
+            Node* right;
+            Node() : value(0), left(nullptr), right(nullptr) {};
+        };
+        Node* root;
+        Node* sortAdd(Node* root, Node* newNode);
+        void printTree(Node*);
+        int height(Node*);
+        bool find(Node*, int);
+        Node* remove(Node*, int);
+        int findMin(Node*);
+        void clear(Node*);
+        Node* leftRotate(Node*);
+        Node* rightRotate(Node*);
+};
+
+#endif
