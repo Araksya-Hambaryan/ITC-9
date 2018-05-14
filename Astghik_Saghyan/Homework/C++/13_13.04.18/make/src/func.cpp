175cac28f123659de171955cce7077117d6ab812
astghiksaghyan <astghiksaghyan@gmail.com>
2018-05-14 17:46:21 +0400

    Changed folder name

--- Astghik_Saghyan/Homework/C++/13_13.04.18/make/src/func.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-void calcSum() {
-    int num1 = 5;
-    int num2 = 6;
-    std::cout << "Sum of " << num1 << " and " << num2;
-    std::cout << " is " << num1+num2 << std::endl; 
-}
--- Astghik_Saghyan/Homework/C++/13_13.04.18/make/src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../inc/test.hpp"
-
-int main() {
-    std::cout << "Make works!" << std::endl;
-    calcSum();    
-    return 0;
-
-}
