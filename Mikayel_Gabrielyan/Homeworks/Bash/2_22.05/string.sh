169f1f06f0abdfd27a8895a32fe672566c3bf878
Mikayel Gabrielyan <mikayel93gabrielyan@gmail.com>
2018-05-23 01:27:45 +0400

    correctet string

--- Mikayel_Gabrielyan/Homeworks/Bash/2_22.05/string.sh
+++ Mikayel_Gabrielyan/Homeworks/Bash/2_22.05/string.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#count="$#"
-#echo "count: $count"
 string=$1
-char=$2
-#aa=$dd${count}
-#echo "aa: $aa"
-echo "count is -> " `awk -F"${char}" '{print NF-1}' <<< "${string}"`
+if [ ${#string} -eq 0 ]; then
+    echo "size of string is 0, please input string argument"
+    exit
+fi
+read -p "input the word -> " char
+if [ ${#char} -eq 0 ] || [ ${#char} -gt ${#string} ]; then
+    echo "size of word is 0 or great then string"
+    exit
+else
+    echo "count is -> " `awk -F"${char}" '{print NF-1}' <<< "${string}"`
+fi
