7ba16ffed479a83a1afafa3f1f7b02a61f6cb2e2
Vahan <vahan.hovhannisyan.1997@gmail.com>
2018-05-25 00:28:56 +0400

    Added functions to Change to zero

--- Vahan_Hovhannisyan/Homeworks/Bash/1_21.05.2018/changeTo0.sh
+++ Vahan_Hovhannisyan/Homeworks/Bash/1_21.05.2018/changeTo0.sh
@@ -1,15 +1,27 @@
 #!/bin/bash/
-for i in {1..10}
-do
 
+generateNums() {
     for i in {1..10}
     do
-        text+=$((RANDOM % 2))
+
+        for i in {1..10}
+        do
+            text+=$((RANDOM % 2))
+        done
+
+        text+="\n"
+
     done
+}
 
-    text+="\n"
+putNumsIntoFule() {
+    printf "$text" > fileBinary
+}
 
-done
-printf "$text" > fileBinary
+replaceZeros() {
+    sed -i '2~2 s/1/0/g' fileBinary # -i means 'sed' makes changes directly in a file
+}
 
-sed -i '2~2 s/1/0/g' fileBinary # -i means 'sed' makes changes directly in a file
+generateNums
+putNumsIntoFule
+replaceZeros
