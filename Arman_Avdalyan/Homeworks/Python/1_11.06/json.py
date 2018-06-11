8f76aeb4032d172a26cd41f2e9bd07fe092fc6b0
Arman <avdalyan1999@gmail.com>
2018-06-12 01:26:45 +0400

    Updated json task

--- Arman_Avdalyan/Homeworks/Python/1_11.06/json.py
+++ Arman_Avdalyan/Homeworks/Python/1_11.06/json.py
@@ -1,34 +1,48 @@
 import re
-# ./json.txt
 
 path = './json.txt'
 
-def isUniqueKey(keys):
+def uniqueKey(keys):
     size = len(keys)
     for i in range(0, size):
         for j in range(i + 1, size):
             if keys[i] == keys[j]:
-                print('Key "', keys[i], '" repeating: ')
+                print('Key "', keys[i], '" is not unique: ')
                 return 0
     return 1
 
-
 def checkValidity(obj):
+    keys = []
     obj = obj.replace(' ', '')
     obj = obj.replace('\n', '')
     obj = obj[1:-1]
+
+    #-------separate json arrays (this elements [....])
+    tmp = obj[obj.find("[")+1:obj.find("]")]
+    tmpArr = tmp.split(',')
+    obj = re.sub(r'\[[^)]*\]', '0', obj)
+    size = len(tmpArr)
+    
+    #-------check item which contains array ["a", "b"]-------
+    for i in range(0, size):
+        if not re.search('"[A-Za-z0-9_]+"|[0-9]+', tmpArr[i]):
+            return 0
+    
+    #-------check json--------------------------------------
     arr = obj.split(',')
     size = len(arr)
-    keys = []
     for i in range(0, size):
-        result1 = re.search('"[A-Za-z0-9\_]+":"[A-Za-z0-9\_]+"', arr[i])
-        result2 = re.search('"[_A-Z_a-z_0-9_]+":[0-9]+$', arr[i])
-        key = re.search('"(.+?)"+?', arr[i]).group(1)
+        result1 = re.search('"[A-Za-z0-9]+":"[A-Za-z0-9_]+"', arr[i])
+        result2 = re.search('"[A-Z_a-z0-9]+":[0-9]+$', arr[i])
+        key = re.search('"(.+?)"+?', arr[i])
+        if key == None:
+            return 0
+        key = key.group(1)
         keys.append(key)
         if result1 == None and result2 == None:
             print('Invalid item is ', arr[i])
             return 0
-    if not isUniqueKey(keys):
+    if not uniqueKey(keys):
         return 0
     return 1
 
@@ -40,7 +54,7 @@ def main():
         myFile = open(path, 'w')
         print('\nPlease open json.txt file from current folder and write there json object: \n')
         return
-    
+
     if checkValidity(obj):
         print('Json is valid: ')
     else:
