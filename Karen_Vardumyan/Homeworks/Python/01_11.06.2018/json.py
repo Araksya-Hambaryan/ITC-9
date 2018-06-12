840e77a36c5347c8bd391544430131fc3c34fc9d
karen <karen@k-desktop.(none)>
2018-06-12 22:50:08 +0400

    in progress

--- Karen_Vardumyan/Homeworks/Python/01_11.06.2018/json.py
+++ Karen_Vardumyan/Homeworks/Python/01_11.06.2018/json.py
@@ -1,33 +1,25 @@
-import json
-from random import choice
-def gen_key():
-    key1 = ''
-    key2 = ''
-
-    leters = ['a','b','c','d','e','f']
-    nums = ['1','2','3','4','5','6','7','8','9']
-
-    while len(key1) != 5:
-	key1 = key1 + choice(leters)
-
-    while len(key2) != 7:
-	key2 = key2 + choice(nums)
-
-    key = {'key1' : key1, 'key2' : key2}
-
-    return key
-
-def creat_json(key_obj):
-    try:
-	data = json.load(open('key.json'))
-    except:
-	data = []
- 
-    data.append(key_obj)
-    with open ('key.json', 'w') as file:
-	json.dump(data,file,indent=2)
+#!/bin/python
 
 
-for i in range(3):
-    creat_json(gen_key())
+import sys
+import re
 
+text = open('text.txt', 'r')
+obj = text.read()
+
+
+obj = obj.replace('\n', '')
+obj = obj.replace(' ', '')
+print(obj)
+
+
+arr = obj.split(',')
+size = len(arr)
+
+print(arr[1])
+num1 = re.search('"A-Za-z0-9":"A-Za-z0-9"', arr[1])
+num2 = re.search('"[A-Z_a-z0-9]+":[0-9]+$', arr[1])
+if num1 == None:
+    print("true")
+else:
+    print("fals")
