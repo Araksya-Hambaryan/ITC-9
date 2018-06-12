25c82f63830291630a50d3bc1c620141db1e5e57
karen <karen@k-desktop.(none)>
2018-06-13 00:57:18 +0400

    task json

--- Karen_Vardumyan/Homeworks/Python/01_11.06.2018/json.py
+++ Karen_Vardumyan/Homeworks/Python/01_11.06.2018/json.py
@@ -1,25 +1,23 @@
 #!/bin/python
 
-
 import sys
 import re
+def change_json():
+    text = open('text.txt', 'r')
+    obj = text.read()
+    obj = obj.replace('\n', '')
+    obj = obj.replace(' ', '')
+    if obj[0] == '{' and obj[-1] == '}':
+	obj = obj[1:-1]
+    else:    
+	return "invalid json"
+    line = obj.split(',')
+    for i in range(0, len(line)):
+	#print(line[i])
+        regex = re.search('"[\w\d]+":"[\w\d]+"', line[i])
+    	if regex == None:
+    	    return "invalid json"
+    return "valid json"
 
-text = open('text.txt', 'r')
-obj = text.read()
 
-
-obj = obj.replace('\n', '')
-obj = obj.replace(' ', '')
-print(obj)
-
-
-arr = obj.split(',')
-size = len(arr)
-
-print(arr[1])
-num1 = re.search('"A-Za-z0-9":"A-Za-z0-9"', arr[1])
-num2 = re.search('"[A-Z_a-z0-9]+":[0-9]+$', arr[1])
-if num1 == None:
-    print("true")
-else:
-    print("fals")
+print(change_json())
