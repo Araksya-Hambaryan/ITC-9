05b6464d0c39ec88e8f43a1b1bb8d14bd2307fec
Elya <elya.karapetyan95@mail.ru>
2018-06-14 00:33:36 +0400

    Update test cases

--- Elya_Karapetyan/Homeworks/Python/1_11-06/testJson.py
+++ Elya_Karapetyan/Homeworks/Python/1_11-06/testJson.py
@@ -4,51 +4,39 @@ import unittest
 class TestJsonFormat(unittest.TestCase):
     def test_checkKey_positive(self):
         keys = ["k1", "k2", "k3"]
-        values = ["value1", 10, "120"]
-        corectJson = '{"key1":"value1","key2":"value2"}'
         result = validateJson.validateKey(keys)
         self.assertEqual(True, result)
 
     def test_checkKey_negative(self):
         keys = ["k1", "k2", "k3"]
-        values = ["value1", 10, "120"]
-        incorectJson = '{"ke y1":"value1","key2":"value2"}'
         result = validateJson.validateKey(keys)
         self.assertEqual(False, result)
     def test_checkQuotesOfKey_negative(self):
         keys = ["k1", "k2", "k3"]
-        values = ["value1", 10, "120"]
-        incorectJson = '{"key1:"value1","key2":"value2"}'
         result = validateJson.validateKey(keys)
         self.assertEqual(False, result)
     def test_checkRepeatingKeys_negative(self):
         keys = ["k1", "k2", "k3"]
-        values = ["value1", 10, "120"]
-        incorectJson = '{"key1":"value1","key1":"value2"}'
         result = validateJson.verifyKeysRepeating(keys)
         self.assertEqual(False, result)
     
     def test_checkCountKeysAndValues_negative(self):
         keys = ["k1", "k2", "k3"]
         values = ["value1", 10, "120"]
-        incorectJson = '{"key1":"value1",:"value2"}'
         result = validateJson.checkCountsOfKeysAndValuesIsEqual(keys, values)
         self.assertEqual(False, result)
 
     def test_checkValue_positive(self):
         values = ["value1", 10, "120"]
-        corectJson = '{"key1":"100","key2":10}'
         result = validateJson.validateValue(values)
         self.assertEqual(True, result)
 
     def test_checkValue_negative(self):
         values = ["value1", 10, "120"]
-        incorectJson = '{"key1":"value1,"key1":"value2"}'
         result = validateJson.validateValue(values)
         self.assertEqual(False, result)
     def test_checkQuotesOfValue_negative(self):
         values = ["value1", 10, "120"]
-        incorectJson = '{"key1":value1,"key1":"value2"}'
         result = validateJson.validateValue(values)
         self.assertEqual(False, result)
     
@@ -73,14 +61,18 @@ class TestJsonFormat(unittest.TestCase):
     
     def test_checkKeyIsEmpty_negative(self):
         keys = ["k1", "k2", "k3"]
-        incorectJson = '{"","value"}'
         result = validateJson.validateKey(keys)
         self.assertEqual(False, result)
 
     def test_checkValueIsEmpty_negative(self):
         values = ["v1", "v2", "v3"]
-        incorectJson = '{"key",""}'
         result = validateJson.validateValue(values)
+        self.assertEqual(False, result)
+    
+    def test_checkJsonIsEmpty_negative(self):
+        values = ["v1", "v2", "v3"]
+        incorectJson = '{}'
+        result = validateJson.isJson(incorectJson, keys, values)
         self.assertEqual(False, result)
 
 if __name__ == '__main__':
