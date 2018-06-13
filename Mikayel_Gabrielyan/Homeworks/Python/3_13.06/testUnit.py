2fcc8b526fdc9f3eb89a84d22113e72b98b8b22e
Mikayel Gabrielyan <mikayel93gabrielyan@gmail.com>
2018-06-14 03:45:13 +0400

    Correscted testUnit

--- Mikayel_Gabrielyan/Homeworks/Python/3_13.06/testUnit.py
+++ Mikayel_Gabrielyan/Homeworks/Python/3_13.06/testUnit.py
@@ -1,10 +1,10 @@
 import unittest
-import validatejson as json
+import jsonValidate as json
 
 exampleForBrackets1 = '{"key1":"value1","key2":"value2"}}'
 exampleForBrackets2 = '"key1":"value1","key2":"value2"}}'
 exampleForBrackets3 = '{"key1":"value1","key2":"value2"'
-class TestcheckBrackets(unittest.TeseCase):
+class TestcheckBrackets(unittest.TestCase):
     def test_brackets1(self):
         result = json.checkBrackets(exampleForBrackets1)
         self.assertTrue(result)
@@ -21,7 +21,7 @@ exampleForCheckCountKeyValue3 = ["key1","key2","key3","key4"]
 exampleForCheckCountKeyValue4 = ["val1","val2","val3"]
 exampleForCheckCountKeyValue5 = ["key1","key2","key3"]
 exampleForCheckCountKeyValue6 = ["val1","val2","val3","val4"]
-class TestcheckCountKeyValueList(unittest.TeseCase):
+class TestcheckCountKeyValueList(unittest.TestCase):
     def test_count1(self):
         result = json.checkCountKeyValueList(exampleForCheckCountKeyValue1,exampleForCheckCountKeyValue2)
         self.assertTrue(result)
@@ -35,7 +35,7 @@ class TestcheckCountKeyValueList(unittest.TeseCase):
 exampleForCheckRepeatKey1 = ["key1","key2","key3","key4"]
 exampleForCheckRepeatKey2 = ["key1","key2","key2","key4"]
 exampleForCheckRepeatKey3 = ["key3","key2","key3","key4"]
-class TestcheckRepeatKey(unittest.TeseCase):
+class TestcheckRepeatKey(unittest.TestCase):
     def test_repeatKey1(self):
         result = json.checkRepeatKey(exampleForCheckRepeatKey1)
         self.assertTrue(result)
