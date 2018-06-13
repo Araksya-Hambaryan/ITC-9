3ff618cefb99e53f669ad3d29465b839c8d23a17
AnushGhazaryan <anush93ghazaryan@gmail.com>
2018-06-13 15:52:44 +0400

    Updated Calculator.py

--- Anush_Ghazaryan/Homeworks/Python/2_12.06/calculator.py
+++ Anush_Ghazaryan/Homeworks/Python/2_12.06/calculator.py
@@ -1,5 +1,32 @@
 
 import math
+def Add(num1,num2):
+    res=num1+num2
+    print('%d + %d = %d ' %( num1, num2, res))
+
+
+def Sub(num1,num2):
+    res=num1-num2
+    print('%d - %d = %d ' %( num1, num2, res))
+    
+
+def Mul(num1,num2):
+    res=num1*num2
+    print('%d * %d = %d ' %( num1, num2, res))
+
+
+def Div(num1,num2):
+    if num2 != 0 :
+        res=num1/num2
+        print('%d / %d = %d ' %( num1, num2, res))
+
+    else:
+        print("The Number Cannot be divided by 0")
+
+def Pow(num1,num2):
+    res=math.pow(num1,num2)
+    print('%d ^ %d = %d ' %( num1, num2, res))
+
 
 def calc():
     num1 = int(input('Input 1st Number: '))
@@ -16,25 +43,16 @@ def calc():
     except NameError :
 	print("There is no such an operator.Try again!")
 
-    if op is 1 :
-	result=num1+num2
-	print('%d + %d = %d ' %( num1, num2, result))
-    elif op is 2 :
-	result=num1-num2
-	print('%d - %d = %d ' %( num1, num2, result))
-    elif op is 3 :
-	result=num1*num2
-	print('%d * %d = %d ' %( num1, num2, result))
-    elif op is 4 :
-	if num2 != 0 :
-	    result=num1/num2
-	    print('%d / %d = %d ' %( num1, num2, result))
-	else :
-	    print("The Number cannot be divided by 0 ")
-    elif op is 5 :
-	result=math.pow(num1,num2)
-	print('%d ^ %d = %d ' %( num1, num2, result))
-
+    if op == 1 :
+        Add(num1,num2)
+    elif op == 2 :
+	Sub(num1,num2)
+    elif op == 3 :
+	Mul(num1,num2)
+    elif op == 4 :
+	    Div(num1,num2)
+    elif op == 5 :
+        Pow(num1,num2)
 def main():
     calc()
     
