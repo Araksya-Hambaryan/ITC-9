d3b4ad238e10d7e3b85c82bba55d337868338a0c
GevorgAntonyan <gevor.antonyan94@gmail.com>
2018-06-14 00:58:44 +0400

    added task

--- Gevorg_Antonyan/Homeworks/Python/11.06/json.py
+++ Gevorg_Antonyan/Homeworks/Python/11.06/json.py
@@ -1,119 +1,124 @@
-#!/bin/python
-######don't add examples with symbols in key or value######
+#!bin/python
 import sys
 import re
 
-obj = open('./text.txt')
-text = obj.read()
-text = text.replace(' ', '')
-text = text.replace('\n', '')
+def primeryListPosition(text, pos1, pos2):
 
-def primeryjson(text1):
-    if text1[0] != '{' or text1[-1] != '}':
-        print('invalid json')
-        exit()
-    text1 = text1[1:-1]
+    for i in range(pos1, pos2):
+        if text[i] == '[':
+            pos1 = i + 1
+            primeryListPosition(text, pos1, pos2)
+        if text[i] == ']':
+            pos1 = pos1 - 1
+            checkPrimeryList(text, pos1, i)      
+    checkPrimeryJson(text, pos1, pos2)    
+    
 
-    for i in range(0, len(text1)):
-        tmp = None
-        if text1[i] == '[':
-            x = i + 1
-            count = len(text1)
-            for j in range(x, count):
-                if text1[j] == '[':
-                    i = j
-                else:
-                    if text1[j] == ']':
-                        symbol = 0
-                        for k in range(i, j):
-                            tmp = tmp + text1[k]
-                            symbol = k
-                        text1 = text1 - tmp
-                        text1[symbol] = '0'
-                        tmp = tmp[1:]
-                        arr = tmp.split(',')
-                        size = len(arr)
-                        for m in range(0, size):
-                            search1 = re.search('[A-Za-z0-9]+', arr[m])
-                            search2 = re.search('[0-9]+', arr[m])
-                            if search1 == None and search2 == None:
-                                print('invalid json')
-                                exit()
-    element = text1.split(',')
-    for i in range(0, len(element)):
+def primeryJsonPosition(text, pos):
+#    if char(text):
+    if text != '0':
+        for i in range(pos, len(text)):
+            if text[i] == '{':
+                pos = i + 1 
+                primeryJsonPosition(text, pos)
+            if text[i] == '}':
+                pos = pos - 1
+                primeryListPosition(text, pos, i)          
+        print("text is ", text)                                       #########################
+        print('invalid json 1')
+        return False
+    else:
+        print('valid json')
+        exit()                                                      ################################
+        return True                                                 
+#    else:
+#    return False
+
+def checkPrimeryList(text, pos1, pos2):
+    pos2 = pos2 - len(text) + 1
+    tmp = text[pos1:pos2] 
+    text = text.replace(tmp, '0')
+    arr = tmp.split(',')
+    size = len(arr)
+    for m in range(0, size):
+        search1 = re.search('[A-Za-z0-9]+', arr[m]) 
+        search2 = re.search('[0-9]+', arr[m])
+        if search1 == None and search2 == None:
+            print('invalid json 2')
+            return False
+    primeryJsonPosition(text, 0)
+
+def checkPrimeryJson(text, pos1, pos2):
+    pos2 = len(text) - pos2 - 1   
+    print("after replace text ", text)
+    tmp = text[pos1:-pos2]
+    print("tmp is ", tmp)                                      #####################) 
+    text = text.replace(tmp, '0')
+    
+    print("after replace text ", text)
+    print("after replace TMP ", tmp)
+    tmp = tmp[1:-1]
+    arr = tmp.split(',')
+    size = len(arr)
+    print("arr is ", arr)                                     ##########################
+    for i in range(0, size):
         search = re.search('"[A-Za-z0-9]+":"[A-Za-z0-9]+"', arr[i])
         search0 = re.search('"[A-Za-z0-9]+":[0-9]+', arr[i])
         if search == None and search0 == None:
-            print('invalid json')
-            exit()
+            print('invalid json 3')
+            return False
+        else:
+            print('valid json')
+#            exit()                                          ###############################
+            return True
+    primeryJsonPosition(text, 0)
+    
 
-
-def jsonInJson(text):
-    if text[0] != '{' or text[-1] != '}':
-        print('invalid json')
-        exit()
-    text = text[1:-1]
-
-    for i in range(0, len(text)):
-        tmp = None
-        if text[i] == '{':
-            x = i + 1
-            count = len(text)
-            for j in range(x, count):
-                if text[j] == '{':
-                    i = j
-                else:
-                    if text[j] == ']':
-                        symbol = 0
-                        for k in range(i, j + 1):
-                            tmp1 = tmp1 + text[k]
-                            primeryjson(tmp1)
-                            symbol = k
-                        text = text - tmp1
-                        text[symbol] = '0'
-
-jsonInJson(text)
-print('valid json')                            
-
-
-#i = 1  
-#line = text.readline()
-#while line:
-#    size = len(line)
-#    count=0
-#    count1=0
-#    count2=0
-#    count3=0
+#def char(text):
+#    size = len(text)
+#    count = 0
+#    count1 = 0
+#    count2 = 0
+#    count3 = 0
+#    count4 = 0
 #    for j in range(0,size):
-#        if line[j] == '"':
-#            count=count+1
-#        if line[j] == ':':
-#            count1=count1+1
-##        if line[j]+line[j+1] == '":':
-##            count2=count2+1
-#        if line[j] == ',':
-#            count3=count3+1
+#        if text[j] == '"':
+#            count=count + 1
+#        if text[j] == '{':
+#            count1=count1 + 1
+#        if text[j] == '}':
+#            count=count2 + 1
+#        if text[j] == '[':
+#            count1=count3 + 1
+#        if text[j] == ']':
+#            count1=count4 + 1
+#
 #    if count%2 > 0:
-#        print("invalid json in line", i)
-#        break
-#    if count1 != count/2:
-#        print("invalid json in line", i)
-#        break
+#        print("invalid json")
+#        return False
 #    if count1 != count2:
-#        print("invalid json in line", i)
-#        break
-#    if count3 != (count2-1):
-#        print("invalid json in line", i)
-#        break
-#    if line[0] != '{':
-#        print("invalid json in line", i)
-#        break
-#    if line[-1] != '}':
-#        print("invalid json in line", i)
-#        break
-#    if line[1] != '"':
-#        print("invalid json in line", i)
-#        break
-#    i=i+1
-#    line = text.readline()
-#text.close()
+#        print("invalid json")
+#        return False
+#    if count3 != count3:
+#        print("invalid json")
+#        return False
+#    return True
+
+
+def main():
+    obj = open('./text.txt')
+    text = obj.read()
+    text = text.replace(' ', '')
+    text = text.replace('\n', '')
+    primeryJsonPosition(text, 0)
+    
+main()
+
+
+
+
+
+
+
+
+
