151424c28a998b615940dbcc32c8d816c50a5863
astghiksaghyan <astghiksaghyan@gmail.com>
2018-06-18 22:34:47 +0400

    Delete node done

--- Astghik_Saghyan/Homeworks/HTML/01_18.06.18/deleteNode/script.js
+++ Astghik_Saghyan/Homeworks/HTML/01_18.06.18/deleteNode/script.js
@@ -1,7 +1,4 @@
 function delElement() {
-    var elem = event.target.value;
-    var node = document.createElement(elem);
-    var text = document.getElementById("text").value;
-    node.innerHTML = text;
-    document.getElementById("newElem").appendChild(node);    
+    var num = document.getElementById("number").value;
+    var span = document.getElementById(num).remove();
 }
