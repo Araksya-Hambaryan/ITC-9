d658742a23fb788596d6d3591d02e073718265e0
stepanchaparyan <stepan@instigatemobile.com>
2018-06-19 17:59:39 +0400

    added classwork about video

---
+++ Classwork/Astghik_Stepan/video/script.js
@@ -0,0 +1,5 @@
+function playVideo() {
+    var source = document.getElementById("video")//.appendChilde("source");
+    console.log(source);
+    source.setAttribute("src", "https://storage.googleapis.com/coverr-main/mp4/The-Hill.mp4");
+}
