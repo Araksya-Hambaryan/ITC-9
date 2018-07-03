4a1c9ab22d9f6851daeb804d69cdd4a3832c1fa9
karen <karen@k-desktop.(none)>
2018-07-03 13:07:00 +0400

    in progress

---
+++ Karen_Vardumyan/Homeworks/Java/01_02.07.2018/Chess.java
@@ -0,0 +1,13 @@
+class Chees {
+    public static void main(String[] args) {
+	Bord bord = new Bord();
+        char [][] newBord = bord.creatBordDef();	
+
+	for (int i=0; i < 26; ++i) {
+	    for(int j=0; j<34; ++j) {
+	        System.out.print(newBord[i][j]) ;     
+	    }
+	    System.out.println();
+        }
+    }
+}
