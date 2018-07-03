298c6c2e147ef7cd1b7594f13c03127eb6dbdd18
astghiksaghyan <astghiksaghyan@gmail.com>
2018-07-03 11:55:36 +0400

    Updated chess

--- Astghik_Saghyan/Homeworks/Java/Board.java
+++ Astghik_Saghyan/Homeworks/Java/Board.java
@@ -1,20 +1,40 @@
 public class Board {
     public char board[][] = new char[8][8];
     Board() {
-    }
- 
-    public void arrangeStones() {
         for (int i = 0; i < 8; i++) {
-            for (int j = 1; j < 8; j++) {
+            for (int j = 0; j < 8; j++) {
                 this.board[i][j]= 'x';
             }
         }
+    }
+ 
+    public void arrangeStones() {
+        for (int j = 0; j < 8; j++) {
+            this.board[1][j]= 'P';
+            this.board[6][j]= 'p';
+        }
+        this.board[0][0]= 'R';
+        this.board[0][7]= 'R';
+        this.board[7][0]= 'r';
+        this.board[7][7]= 'r';
+        this.board[0][1]= 'N';
+        this.board[0][6]= 'N';
+        this.board[7][1]= 'n';
+        this.board[7][6]= 'n';
+        this.board[0][2]= 'B';
+        this.board[0][5]= 'B';
+        this.board[7][2]= 'b';
+        this.board[7][5]= 'b';
+        this.board[0][3]= 'Q';
+        this.board[0][4]= 'K';
+        this.board[7][3]= 'k';
+        this.board[7][4]= 'q';
     }
     public void printBoard() {
         System.out.println();
         for (int i = 0; i < 8; i++) {
             System.out.print(i+1);
-            System.out.print(" ");
+            System.out.print("  ");
             for (int j = 0; j < 8; j++) {
                 System.out.print(this.board[i][j]);
                 System.out.print(" ");
@@ -22,7 +42,7 @@ public class Board {
             System.out.println();
         }
         System.out.println();
-        System.out.println("   A B C D E F G");
+        System.out.println("   A B C D E F G H");
         System.out.println();
     }
 }
