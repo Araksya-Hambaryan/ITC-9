8922cbc619a29d8e69c104ef6e616ec63c7685f6
Mikayel Gabrielyan <mikayel93gabrielyan@gmail.com>
2018-07-07 16:32:19 +0400

    Done Annotation

--- Mikayel_Gabrielyan/Homeworks/Java/2_06.07/Main.java
+++ Mikayel_Gabrielyan/Homeworks/Java/2_06.07/Main.java
@@ -3,10 +3,17 @@ import java.lang.reflect.*;
 public class Main {
     public static void main(String[] args) {
 
-        Annotation annTest = new Annotation();  
-        Method m1 = annTest.getClass().getDeclaredMethod("method1");  
-        MyAnnotation myAnnotation = m1.getAnnotation(MyAnnotation.class);  
-        System.out.println("MyAnnotation value is: " + myAnnotation.value());
+        try {
+            Annotation annTest = new Annotation();  
+            Method m1 = annTest.getClass().getDeclaredMethod("method1");  
+            MyAnnotation myAnnotation1 = m1.getAnnotation(MyAnnotation.class);  
+            System.out.println("MyAnnotation value is: " + myAnnotation1.value());
+            Method m2 = annTest.getClass().getDeclaredMethod("method2");  
+            MyAnnotation myAnnotation2 = m2.getAnnotation(MyAnnotation.class);  
+            System.out.println("MyAnnotation value is: " + myAnnotation2.value());
+        } catch (NoSuchMethodException ex) {
+            System.out.println(ex);
+        }
 
     }
 }
