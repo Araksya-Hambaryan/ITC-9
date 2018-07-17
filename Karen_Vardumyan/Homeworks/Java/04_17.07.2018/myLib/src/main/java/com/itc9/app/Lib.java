deb6eb869171e9270d3ba6bf5ef88f2d16b70f45
karen <karen@k-desktop.(none)>
2018-07-17 23:29:20 +0400

    in progress

---
+++ Karen_Vardumyan/Homeworks/Java/04_17.07.2018/myLib/src/main/java/com/itc9/app/Lib.java
@@ -0,0 +1,34 @@
+package com.itc9.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lib {
+    public static void readFile(String fileName) {
+		System.out.println("HOME");
+
+//		String fileName = "myFile.txt";
+//
+//        String line = null;
+//
+//        try {
+//            FileReader fileReader = new FileReader(fileName);
+//
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//
+//            while((line = bufferedReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//            bufferedReader.close();
+//
+//        }
+//        catch(FileNotFoundException ex) {
+//            System.out.println("Unable to open file '" + fileName + "'");
+//        }
+//
+//        catch(IOException ex) {
+//            System.out.println("Error reading file '" + fileName + "'");
+//        }
+    }
+}
---
+++ Karen_Vardumyan/Homeworks/Java/04_17.07.2018/myProject/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import com.itc9.app.myLib
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+		myLib.readFile("hello");
+    }
+}
