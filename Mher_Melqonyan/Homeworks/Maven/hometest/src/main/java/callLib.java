6cd363043028238f070c0b6a16f466d128834c57
mher <mmelqonyan1990@gmail.com>
2018-07-18 01:30:09 +0400

    Maven

---
+++ Mher_Melqonyan/Homeworks/Maven/hometest/src/main/java/callLib.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static hometest.tst.*;
+
+
+public class callLib {
+@Test
+    public void test(){
+
+   Assert.assertEquals(returnSomeString(), returnSomeString2());
+    System.out.println("sucsess");
+}
+
+
+
+}
---
+++ Mher_Melqonyan/Homeworks/Maven/hometest/src/main/java/tst.java
@@ -0,0 +1,16 @@
+package hometest;
+public class tst {
+
+    public static String returnSomeString() {
+
+        return "some string";
+
+    }
+
+    public static String returnSomeString2() {
+
+        return "some string";
+
+    }
+
+}
