af1a09c971360155bd58b15d5deb986ac34d3dad
astghiksaghyan <astghiksaghyan@gmail.com>
2018-07-17 18:03:43 +0400

    Maven project in progress

---
+++ Astghik_Saghyan/Homeworks/Maven/custom-lib/src/main/java/com/itc/myLib/App.java
@@ -0,0 +1,9 @@
+package com.itc.myLib;
+
+public class App 
+{
+    public static void show()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Astghik_Saghyan/Homeworks/Maven/myProject/src/main/java/com/itc9/general/Test.java
@@ -0,0 +1,9 @@
+package com.itc9.general;
+
+public class Test
+{
+    public static void main( String[] args )
+    {
+        App.show();
+    }
+}
