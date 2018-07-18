ca73459ae094bddd69b5139d2ef45d450ce2ee58
ElyaKarapetyan <elya.karapetyan95@mail.ru>
2018-07-18 14:44:13 +0400

    delete useless files

--- Elya_Karapetyan/Homeworks/Maven/1_16-07/json-diff/src/main/java/com/itc/jsonDiff/App.java
+++ Elya_Karapetyan/Homeworks/Maven/1_16-07/json-diff/src/main/java/com/itc/jsonDiff/App.java
@@ -3,25 +3,23 @@ package com.itc.jsonDiff;
 import com.itc.myCustomLib.FileProcessor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
+import org.json.*;
 import java.io.IOException;
 
-public class App 
-{
-    public static void main(String[] args) throws IOException
-    {
-	String firstJson = " ";
-	String firstPath = "../json-diff/resurses/firstJson.txt"; 
-	firstJson = FileProcessor.readFile(firstPath, firstJson);
-	String secondJson = " ";
-	String secondPath = "../json-diff/resurses/secondJson.txt"; 
-	secondJson = FileProcessor.readFile(secondPath, secondJson);
-	//JSONObject firstObj = new JSONObject(firstJson);
-	//JSONObject secondObj = new JSONObject(secondJson);
-	if (FileProcessor.compareTwoString(firstJson, secondJson)) {
-	    System.out.println("\n\nString is equales!\n\n");
-	} else {
-	    System.out.println("\n\nString is not equales!\n\n");
+public class App {
+    public static void main(String[] args) throws IOException {
+		String firstJson = " ";
+		String firstPath = "../json-diff/resurses/firstJson.txt"; 
+		firstJson = FileProcessor.readFile(firstPath, firstJson);
+		String secondJson = " ";
+		String secondPath = "../json-diff/resurses/secondJson.txt"; 
+		secondJson = FileProcessor.readFile(secondPath, secondJson);
+		//JSONObject firstObj = new JSONObject(firstJson);
+		//JSONObject secondObj = new JSONObject(secondJson);
+		if (FileProcessor.compareTwoString(firstJson, secondJson)) {
+			System.out.println("\n\nString is equales!\n\n");
+		} else {
+			System.out.println("\n\nString is not equales!\n\n");
+		}
 	}
-    }
 }
