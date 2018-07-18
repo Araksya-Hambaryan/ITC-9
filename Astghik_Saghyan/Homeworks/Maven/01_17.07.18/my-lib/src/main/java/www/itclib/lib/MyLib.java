4e49199ead28b6e39642a1d39b434039c9e6f05a
astghiksaghyan <astghiksaghyan@gmail.com>
2018-07-18 14:59:14 +0400

    FileProc in progress

---
+++ Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-lib/src/main/java/www/itclib/lib/MyLib.java
@@ -0,0 +1,14 @@
+package www.itclib.lib;
+
+public class MyLib 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        fileProc();
+    }
+
+    public static void fileProc() {
+        System.out.println( "fileProc called!" );
+    }
+}
