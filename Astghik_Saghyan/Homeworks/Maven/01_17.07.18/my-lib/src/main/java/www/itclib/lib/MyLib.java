2b06388723b1ed6adf0f41f79ef6d042ca1e10e0
astghiksaghyan <astghiksaghyan@gmail.com>
2018-07-20 00:30:29 +0400

    updated

--- Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-lib/src/main/java/www/itclib/lib/MyLib.java
+++ Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-lib/src/main/java/www/itclib/lib/MyLib.java
@@ -6,11 +6,6 @@ import java.io.IOException;
 
 public class MyLib 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
     public void fileProc(String fileName) {
         System.out.println( "FileProc called!"+fileName );
         
