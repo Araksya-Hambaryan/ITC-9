00fc34b7f739be787cf589b0cd9ba812faaf81b7
astghiksaghyan <astghiksaghyan@gmail.com>
2018-07-19 22:47:11 +0400

    FileProc in progress

--- Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-app/src/main/java/com/itc9/app/App.java
+++ Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-app/src/main/java/com/itc9/app/App.java
@@ -14,6 +14,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        fileProc();
+        fileProc("input.txt");
     }
 }
--- Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-lib/src/main/java/www/itclib/lib/MyLib.java
+++ Astghik_Saghyan/Homeworks/Maven/01_17.07.18/my-lib/src/main/java/www/itclib/lib/MyLib.java
@@ -5,10 +5,21 @@ public class MyLib
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        fileProc();
     }
 
-    public static void fileProc() {
+    public static void fileProc(String fileName) {
         System.out.println( "fileProc called!" );
+          try {
+            File file = new File(fileName);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
