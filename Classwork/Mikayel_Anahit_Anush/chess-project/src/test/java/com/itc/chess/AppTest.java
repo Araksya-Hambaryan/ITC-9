01a89c1905d79a50bd03b611b7ae7da1c4ce36a8
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-07-19 22:31:04 +0400

    Updated chess-project

--- Classwork/Mikayel_Anahit_Anush/chess-project/src/test/java/com/itc/chess/AppTest.java
+++ Classwork/Mikayel_Anahit_Anush/chess-project/src/test/java/com/itc/chess/AppTest.java
@@ -1,5 +1,7 @@
 package com.itc.chess;
+
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -7,51 +9,50 @@ import org.testng.annotations.Test;
  * Unit test for simple App.
  */
 public class AppTest {
-    @Test
-    public static void checkBishopWayTestng() {
-        Assert.assertTrue(com.itc.chess.Bishop.checkBishopWay(0, 5, 1, 4));
-        System.out.println("Bishop's step is True");
-    }
-
-
-    @Test
-    public static void checkHorseWayTestng() {
-        Assert.assertTrue(com.itc.chess.Horse.checkHorseWay(1, 2, 2, 4));
-        System.out.println("Hourse's step is True");
-    }
-
-
-    @Test
-    public static void checkKingWayTestng() {
-        Assert.assertTrue(com.itc.chess.King.checkKingWay(3, 6, 4, 5));
-        System.out.println("King's step is True");
-    }
-
-    @Test
-    public static void checkQueenWayTestng(){
-        Assert.assertTrue(com.itc.chess.Queen.checkQueenWay(3,3,6,6));
-        System.out.println("Queen's step is True ");
-    }
-    @Test
-    public static void checkRookWayTestng(){
-        Assert.assertFalse(com.itc.chess.Rook.checkRookWay(1,1,1,5));
-        System.out.println("Rook's step is False");
-    }
-
-
-    @Test
-    public static void getSymbolBoard(){
-        Board.defaultBoard();
-        Assert.assertEquals(com.itc.chess.Board.getSimb(0,0),'\u265C');
-        System.out.println("Rook's coordiante");
-    }
-
-    @Test
-    public static void addFigure(){
+    @BeforeMethod
+    public void beforeAllMethods(){
         Board.defaultBoard();
         Board.add(4,5,'\u265B');
-        Assert.assertEquals(com.itc.chess.Board.getSimb(4,5), '\u265B');
-        System.out.println("Add Figure");
+    }
+    @Test
+    public  void checkBishopWayTestng() {
+        Assert.assertTrue(Bishop.checkBishopWay(0, 5, 1, 4));
+
+    }
+
+
+    @Test
+    public  void checkHorseWayTestng() {
+        Assert.assertTrue(Horse.checkHorseWay(1, 2, 2, 4));
+
+    }
+
+
+    @Test
+    public  void checkKingWayTestng() {
+        Assert.assertTrue(King.checkKingWay(3, 6, 4, 5));
+
+    }
+
+    @Test
+    public void checkQueenWayTestng(){
+        Assert.assertTrue(Queen.checkQueenWay(3,3,6,6));
+    }
+    @Test
+    public void checkRookWayTestng(){
+        Assert.assertFalse(Rook.checkRookWay(1,1,1,5));
+    }
+
+
+    @Test
+    public void getSymbolBoard(){
+
+        Assert.assertEquals(Board.getSimb(0,0),'\u265C');
+    }
+
+    @Test
+    public void addFigure(){
+        Assert.assertEquals(Board.getSimb(4,5), '\u265B');
 
     }
 
