02c913417a15be5dbba61a438b1be56c6051cb1e
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-07-22 01:22:09 +0400

    Added

---
+++ Anush_Ghazaryan/Homeworks/Android/1_21.07/seekBarColor2/app/src/main/java/com/example/anush/seekbarcolor/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.anush.seekbarcolor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.constraint.ConstraintLayout;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBarRed;
+    private SeekBar seekBarBlue;
+    private SeekBar seekBarGreen;
+
+    private TextView textViewCompatOne;
+    private TextView textViewCompatTwo;
+    private TextView textViewCompatThree;
+
+
+
+
+}
