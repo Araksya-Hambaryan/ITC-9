6701b8e4ec851fd3310ebefa0b8efc83fbaf6c1d
KarenVardumyan <karenvardumyan94@gmail.com>
2018-08-27 22:46:29 +0400

    in progres task Armenian Heroes

---
+++ Karen_Vardumyan/Homeworks/Javascript/01_27.08.2018/ArmenianHeroes/script.js
@@ -0,0 +1,43 @@
+
+function foo(id) {
+    var title = document.getElementById("title");
+    var image = document.getElementById("image");
+    switch (id) {
+        case "monte":
+            title.innerHTML = monte;
+            image.src = "./ArmenianHeroes/monte.jpg";
+            break;
+        case "vardan":
+            title.innerHTML = vardan;
+            image.src = "./ArmenianHeroes/vardan.jpg";
+            break;
+        case "tatul":
+            title.innerHTML = tatul;
+            image.src = "./ArmenianHeroes/tatul.jpg";
+            break;
+
+        case "vazgen":
+            title.innerHTML = vazgen;
+            image.src = "./ArmenianHeroes/vazgen.jpg"
+            break;
+        case "garegin":
+            title.innerHTML = garegin;
+            image.src = "./ArmenianHeroes/njdeh.jpg";
+            break;
+    }
+}
+
+var monte = "Monte Melkonian was an Armenian revolutionary,[1] left-wing nationalist militant and commander. He was the leader of an offshoot of the Armenian Secret Army for the Liberation of Armenia (ASALA) in the 1980s and the most celebrated commander during the Nagorno-Karabakh War in the early 1990s.[2]\n" +
+    "\n" +
+    "An Armenian-American, Melkonian left the United States and arrived in Iran in 1978 during the beginning of the 1979 Revolution, taking part in demonstrations against the Shah. Following the collapse of the Shah's monarchy, he traveled to Lebanon during the height of the civil war and served in an Armenian militia group in the Beirut suburb of Bourj Hammoud. In ASALA, he took part in the assassinations of several Turkish diplomats in Europe during the early to mid-1980s. He planned the 1981 Turkish consulate attack in Paris.[3] He was later arrested and sent to prison in France. In 1989, he was released and in the following year, acquired a visa to travel to Armenia.";
+var vardan = "Vardan Stepanyan was born on March 9, 1966 in Yerevan, Soviet Armenia.[9] After finishing the Tumanyan School #32 of Yerevan in 1983,[2] he was drafted to the Soviet army. He served in Afghanistan from 1984 to 1986. For his service there, Stepanyan was awarded with Medal \"For Courage\" and Jubilee Medal \"70 Years of the Armed Forces of the USSR\". After being discharged from the Red army, Stepanyan was admitted into the Yerevan State University Department of Law.[2]";
+var tatul = "Tatul Krpeyan was an Armenian commander. He was the self-appointed leader of paramilitary units in Getashen and Martunashen villages in Shahumyan Region of Soviet Azerbaijan during the Nagorno-Karabakh War. He was killed during Operation Ring by the Soviet Azerbaijani OMON (special police). Born in the village of Areg near the town of Talin, Armenia, he completed his elementary and intermediary education in the Technical School of Talin. After serving in the Soviet army, in 1987 he entered the Faculty of History of Yerevan State University. In his first year at the university, Tatul joined the Armenian-liberation movement. He was also a member of the \"Miatsum\" (Unification) organization, whose efforts were directed to unite Nagorno-Karabakh Autonomous Oblast with Armenia.";
+var vazgen = "Vazgen Sargsyan was an Armenian military commander and politician. He was the first Defence Minister of Armenia from 1991 to 1992 and then from 1995 to 1999. He served as Armenia's Prime Minister from 11 June 1999 until his assassination on 27 October of that year. He rose to prominence during the mass movement for the unification of Nagorno-Karabakh with Armenia in the late 1980s and led Armenian volunteer groups during the early clashes with Azerbaijani forces. Appointed Defence Minister by President Levon Ter-Petrosyan soon after Armenia's independence from the Soviet Union in late 1991, Sargsyan became the most prominent commander of Armenian forces during the Nagorno-Karabakh War.[2] In different positions, he regulated the military operations in the war area until 1994, when a ceasefire was reached ending the war with the de facto unification of Nagorno-Karabakh Republic with Armenia.\n" +
+    "\n";
+var garegin = "Garegin Ter-Harutyunyan (1 January 1886 – 21 December 1955) was an Armenian statesman and military strategist. As a member of the Armenian Revolutionary Federation, he was involved in national liberation struggle and revolutionary activities during the First Balkan War and World War I. Garegin Nzhdeh was one of the key political and military leaders of the First Republic of Armenia (1918–1921), and is widely admired as a charismatic national hero by Armenians.[1][2]\n" +
+    "\n" +
+    "In 1921, he instrumented the establishment of the Republic of Mountainous Armenia, an anti-Bolshevik state that became a key factor that led to the inclusion of the province of Syunik into Soviet Armenia.[3][4] During World War II, he assisted the Armenian Legion of the Wehrmacht, the armed forces of Nazi Germany, he hoped that if Germany succeeded in conquering the USSR, they would be able to grant Armenia independence.";
+
+
+
+
