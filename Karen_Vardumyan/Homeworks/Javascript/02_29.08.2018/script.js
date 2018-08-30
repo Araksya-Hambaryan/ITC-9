c4e286c5dddc8ea9294121a24345bfeaac9262ff
KarenVardumyan <karenvardumyan94@gmail.com>
2018-08-30 23:54:40 +0400

    in progres loto

--- Karen_Vardumyan/Homeworks/Javascript/02_29.08.2018/script.js
+++ Karen_Vardumyan/Homeworks/Javascript/02_29.08.2018/script.js
@@ -1,179 +1,302 @@
 
 
-var ticket1 = [ ["","","","","","","","",""],
-                ["","","","","","","","",""],
-                ["","","","","","","","",""] ]
 
+var t1 = [];
+var t2 = [];
+var t3 = [];
 
+var ticket1 = [];
+var ticket2 = [];
+var ticket3 = [];
 
-var ticket2 = [ ["","","","","","","","",""],
-                ["","","","","","","","",""],
-                ["","","","","","","","",""] ]
-
-var ticket3 = [ ["","","","","","","","",""],
-                ["","","","","","","","",""],
-                ["","","","","","","","",""] ]
-
-
-var numbers = ["", "", "", "", ""];
-
+var numbers = [];
 
 function creatnumbers() {
+
+    numbers = getnumbersArray();
+
     var table = document.getElementById("numbers");
     while (table.firstChild) {
         table.removeChild(table.firstChild);
     }
-
     var table = document.getElementById("numbers");
     var tableRow = document.createElement("tr");
     for(let j = 0; j < numbers.length ; j++) {
-        numbers[j] = Math.floor(Math.random() * 20) + 1;
         var txt = document.createTextNode(numbers[j]);
         var tableData = document.createElement("td");
         tableData.appendChild(txt);
+        tableData.style.color = "#c97819";
         tableRow.appendChild(tableData);
         table.appendChild(tableRow);
     }
-
-    // for(let i = 0; i < ticket1.length; i++) {
-    //     for(let j = 0; j < ticket1[i].length ; j++) {
-    //         if(ticket1[i][j] === numbers[i]) {
-    //
-    //             console.log(ticket1);
-    //
-    //             var x = document.getElementById("ticket1").getElementsByTagName("td");
-    //             // x[0].innerHTML = "i want to change my cell color";
-    //             x[j].style.backgroundColor = "yellow";
-    //             x[9 + j].style.backgroundColor = "yellow";
-    //             x[18 + j].style.backgroundColor = "yellow";
-    //
-    //         }
-    //     }
-    // }
     return numbers;
-
 }
 
-function steep() {
-    var num = creatnumbers();
 
-    console.log(num);
-    console.log(ticket1[0]);
-    console.log(ticket1[1]);
 
-    for(let i = 0; i < 3; ++i ) {
-        for(let j = 0; j < 9; ++j) {
-            for(let k = 0; k < 5; k++) {
-                if(ticket1[i][j] == num[k]) {
-                    console.log("&&&&&&&&&&&&&&&&&&&&&&&&7");
+//////////////////// in progresss ////////////////////
 
-                    var x = document.getElementById("ticket1").getElementsByTagName("td");
-                    x[j].style.backgroundColor = "yellow";
-                }
-            }
+
+// function counter(t1,id,counter) {
+//     for(let i = 0; i < t1[0].length; ++i) {
+//
+//         var x = document.getElementById(id).getElementsByTagName("td");
+//         if(x[i].color == "yellow") {
+//             var x = document.getElementById(id).getElementsByTagName("td");
+//             counter++;
+//         }
+//     }
+//     for(let i = 0; i < t1[0].length; ++i) {
+//         var x = document.getElementById(id).getElementsByTagName("td");
+//         if(x[i+9].color == "yellow") {
+//             counter++;
+//         }
+//     }
+//     for(let i = 0; i < t1[0].length; ++i) {
+//         var x = document.getElementById(id).getElementsByTagName("td");
+//         if(x[i+18].color == "yellow") {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
+
+
+function changeColor(t1,id) {
+    for(let i = 0; i < t1[0].length; ++i) {
+        if(t1[0][i] === numbers[num]) {
+            var x = document.getElementById(id).getElementsByTagName("td");
+            x[i].style.color = "yellow";
+        }
+    }
+    for(let i = 0; i < t1[0].length; ++i) {
+        if(t1[1][i] === numbers[num]) {
+            var x = document.getElementById(id).getElementsByTagName("td");
+            x[i+9].style.color = "yellow";
+        }
+    }
+    for(let i = 0; i < t1[0].length; ++i) {
+        if(t1[2][i] === numbers[num]) {
+            var x = document.getElementById(id).getElementsByTagName("td");
+            x[i+18].style.color = "yellow";
         }
     }
 }
 
 
+
+num = 0;
+counterT1 = 0;
+counterT2 = 0;
+counterT3 = 0;
+function openNumbers() {
+    openOneNumber(num);
+    changeColor(t1,"ticket1");
+    // counterT1 = counter(t1,"ticket1",counterT1);
+    // console.log("----------------   " + counterT1);
+    changeColor(t2,"ticket2");
+    changeColor(t3,"ticket3");
+    if (num !== 4) {
+        num++;
+    } else {
+        num = 0;
+    }
+}
+function openOneNumber(num) {
+    var x = document.getElementById("numbers").getElementsByTagName("td");
+    x[num].style.color = "yellow";
+}
+
+
+
+
+
+
+//
+// function steep() {
+//     var num = creatnumbers();
+// }
+
+
+function getnumbersArray() {
+    var nums = [];
+    for(let k = 1; k < 91; ++k) {
+        nums.push(k);
+    }
+    ranNums = [],
+    i = nums.length,
+    j = 0;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        ranNums.push(nums[j]);
+        nums.splice(j,1);
+    }
+    var elem = [];
+    for(let i = 0; i < 5; ++i) {
+        elem[i] = ranNums[i];
+    }
+    elem.sort();
+    return elem;
+}
 
 
 
 function foo() {
-    creatT1();
-    creatT2();
-    creatT3()
+    t1 = creatTicket(ticket1,"ticket1");
+    t2 = creatTicket(ticket2,"ticket2");
+    t3 = creatTicket(ticket3,"ticket3");
 }
 
-function creatT1() {
-    var table = document.getElementById("ticket1");
-    for(let i = 0; i < ticket1.length; i++) {
-        var tableRow = document.createElement("tr");
-        for(let j = 0; j < ticket1[i].length ; j++) {
 
-
-
-
-
-            // var rundom = new Set();
-            // for (let i = 0; i < 6; i++) {
-            //     rundom[i] = Math.floor(Math.random() * 8) + 1;
-            // }
-            // //console.log(rundom);
-            //
-            // ticket1[i][rundom[j]] = Math.floor(Math.random() * 20) + 1;
-
-            ticket1[i][j] = Math.floor(Math.random() * 20) + 1;
-
-
-            var txt = document.createTextNode(ticket1[i][j]);
-            var tableData = document.createElement("td");
-            tableData.appendChild(txt);
-            tableRow.appendChild(tableData);
-        }
-        table.appendChild(tableRow);
+function creatTicket(ticket1,id) {
+    ticket1 = [ [,,,,,,,,,],[,,,,,,,,,],[,,,,,,,,,] ];
+    var table = document.getElementById(id);
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
     }
-}
 
+    //1 line
+    var tableRow = document.createElement("tr");
+    var positions = getPositions();
+    for(let i = 0; i < 5; ++i) {
+        ticket1[0][positions[i]] = (Math.floor(Math.random() * 9) + 1) + (positions[i]*10);
+    }
+    for(let j = 0; j < ticket1[0].length ; j++) {
+        if(ticket1[0][j] !== undefined) {
+            var txt = document.createTextNode(ticket1[0][j]);
+        } else {
+            var txt = document.createTextNode(" ");
+        }
+        var tableData = document.createElement("td");
+        tableData.appendChild(txt);
+        tableRow.appendChild(tableData);
+    }
+    table.appendChild(tableRow);
 
-
-
-
-
-
-function creatT2() {
-    var table = document.getElementById("ticket2");
-    for(let i = 0; i < ticket2.length; i++) {
-        var tableRow = document.createElement("tr");
-        for(let j = 0; j < ticket2[i].length ; j++) {
-
-            var rundom = new Set();
-            for (let i = 0; i < 6; i++) {
-                rundom[i] = Math.floor(Math.random() * 8) + 1;
+    //line 2
+    var tableRow = document.createElement("tr");
+    var positions = getPositions();
+    for(let i = 0; i < 5; ++i) {
+         while (true) {        // for(let k = 0; k < 100; k++){
+            ticket1[1][positions[i]] = (Math.floor(Math.random() * 9) + 1) + (positions[i] * 10);
+            if (ticket1[0][positions[i]] !== ticket1[1][positions[i]] && ticket1[1][positions[i]] !== undefined) {
+                break;
             }
-            //console.log(rundom);
-
-            ticket2[i][rundom[j]] = Math.floor(Math.random() * 20) + 1;
-
-            var txt = document.createTextNode(ticket2[i][j]);
-            var tableData = document.createElement("td");
-            tableData.appendChild(txt);
-            tableRow.appendChild(tableData);
         }
-        table.appendChild(tableRow);
     }
-}
+    for(let j = 0; j < ticket1[1].length ; j++) {
+        if(ticket1[1][j] !== undefined) {
+            var txt = document.createTextNode(ticket1[1][j]);
+        } else {
+            var txt = document.createTextNode(" ");
+        }
+        var tableData = document.createElement("td");
+        tableData.appendChild(txt);
+        tableRow.appendChild(tableData);
+    }
+    table.appendChild(tableRow);
 
-function creatT3() {
-    var table = document.getElementById("ticket3");
-    for(let i = 0; i < ticket3.length; i++) {
-        var tableRow = document.createElement("tr");
-        for(let j = 0; j < ticket3[i].length ; j++) {
-
-            var rundom = new Set();
-            for (let i = 0; i < 6; i++) {
-                rundom[i] = Math.floor(Math.random() * 8) + 1;
+    //line 3
+    var empty = [];
+    for(let i = 0; i < 9; ++i) {
+        if ((ticket1[0][i] === ticket1[1][i] && ticket1[1][i] === undefined))  {
+            empty.push(i);
+        }
+    }
+    for(let i = 0; i < empty.length; ++i) {
+        ticket1[2][empty[i]] = (Math.floor(Math.random() * 9) + 1) + (empty[i]*10);
+    }
+    var result = arrDiff(empty);
+    var last = getPositionsLast(result,empty.length);
+    var tableRow = document.createElement("tr");
+    for(let i = 0; i < last.length - 1; ++i) {
+        while (true) {        // for(let k = 0; k < 100; k++){
+            ticket1[2][last[i]] = (Math.floor(Math.random() * 9) + 1) + (last[i] * 10);
+            if ((ticket1[2][last[i]] !== ticket1[1][last[i]])  && (ticket1[2][last[i]] !== ticket1[0][last[i]])
+                && ticket1[2][last[i]] !== undefined) {
+                break;
             }
-            //console.log(rundom);
-
-            ticket3[i][rundom[j]] = Math.floor(Math.random() * 20) + 1;
-
-
-            var txt = document.createTextNode(ticket3[i][j]);
-            var tableData = document.createElement("td");
-            tableData.appendChild(txt);
-            tableRow.appendChild(tableData);
         }
-        table.appendChild(tableRow);
     }
+    for(let j = 0; j < ticket1[2].length ; j++) {
+        if(ticket1[2][j] !== undefined) {
+            var txt = document.createTextNode(ticket1[2][j]);
+        } else {
+            var txt = document.createTextNode(" ");
+        }
+        var tableData = document.createElement("td");
+        tableData.appendChild(txt);
+        tableRow.appendChild(tableData);
+    }
+    table.appendChild(tableRow);
+
+
+    return ticket1;
 }
 
 
-function changeColor() {
-    var x = document.getElementById("ticket1").getElementsByTagName("td");
-    // x[0].innerHTML = "i want to change my cell color";
-    x[19].style.backgroundColor = "yellow";
+
+
+
+
+
+function getPositions() {
+    var nums = [0,1,2,3,4,5,6,7,8],
+        ranNums = [],
+        i = nums.length,
+        j = 0;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        ranNums.push(nums[j]);
+        nums.splice(j,1);
+    }
+    var elem = [];
+    for(let i = 0; i < 5; ++i) {
+        elem[i] = ranNums[i];
+    }
+    elem.sort();
+    return elem;
 }
 
 
-//console.log(Math.floor(Math.random() * 20));
+function getPositionsLast(nums,count) {
+        ranNums = [],
+        i = nums.length,
+        j = 0;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        ranNums.push(nums[j]);
+        nums.splice(j,1);
+    }
+    var elem = [];
+    for(let i = 0; i < 6 - count; ++i) {
+        elem[i] = ranNums[i];
+    }
+    elem.sort();
+    return elem;
+}
+
+
+function arrDiff (a2) {
+    var a1 = [1,2,3,4,5,6,7,8,9];
+
+    var a = [], diff = [];
+    for (var i = 0; i < a1.length; i++) {
+        a[a1[i]] = true;
+    }
+    for (var i = 0; i < a2.length; i++) {
+        if (a[a2[i]]) {
+            delete a[a2[i]];
+        } else {
+            a[a2[i]] = true;
+        }
+    }
+    for (var k in a) {
+        diff.push(k);
+    }
+    return diff;
+}
+
+
+
