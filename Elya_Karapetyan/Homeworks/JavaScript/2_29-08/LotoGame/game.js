2c7b826eead31fb56d6f078f4a476fa808cb1b5f
AnahitKaryan <1anahit1@mail.ru>
2018-08-30 13:37:55 +0400

    Update game

--- Elya_Karapetyan/Homeworks/JavaScript/2_29-08/LotoGame/game.js
+++ Elya_Karapetyan/Homeworks/JavaScript/2_29-08/LotoGame/game.js
@@ -1,7 +1,18 @@
+const row = 3;
+const column = 9;
+let matrix;
+
+function findCardTable(id) {
+    return document.getElementById(id);    
+}
+
+function generateRandomNumber(max, min) {
+    const number = Math.floor((Math.random() * (max - min) + min));
+    return number;
+ }
+
 function createLotoCard(id) {
-    const lotoCard = document.getElementById(id);    
-    const row = 3;
-    const column = 9;
+    const lotoCard = findCardTable(id);
     lotoCard.setAttribute("border", "1");
     for (let i = 0; i < row; i++) {
         let tr = document.createElement("tr");
@@ -10,17 +21,54 @@ function createLotoCard(id) {
             td.height = 40;
             td.width = 60;
             tr.appendChild(td); 
-            td.innerText = generateRandomNumber();           
         }              
         lotoCard.appendChild(tr); 
     }
-  }
-
-function generateRandomNumber() {
-   const number = Math.floor((Math.random() * 90));
-   return number;
 }
-  
-createLotoCard("firstLotoCard");
-createLotoCard("secondLotoCard");
-createLotoCard("thirdLotoCard");
+
+function generateRandomMatrix() {
+    matrix = new Array(row);
+    for (let i = 0; i < row; ++i) {
+        let min = 0;
+        let max = 10;
+
+        matrix[i] = new Array(column);
+        for (let j = 0; j < column; ++j) {
+            if (j === 0) { 
+                matrix[i][j] = generateRandomNumber(1, 9);
+            } 
+            if (j === max/10) {
+                min += 10
+                max += 10;
+                   matrix[i][j] = generateRandomNumber(min, max);
+            } 
+        }
+    }
+}
+
+function setNumbersInLotoCard(id) {
+    createLotoCard(id);
+    const oneCard = findCardTable(id);
+    generateRandomMatrix();
+    
+    let numberCount = 15;
+    let rowMaxCount = 2;
+    let columnMaxCount = 5;
+    let rowIndexArray = new Array(row);
+    let columnIndexArray = new Array(column);
+    let i = 0;
+    let j = 0;
+    while (numberCount > 0) {
+        rowIndexArray[i] = generateRandomNumber(0, 3);
+        columnIndexArray[j] = generateRandomNumber(0, 9);
+        oneCard.rows[row].cells[column].innerText = matrix[row][column];
+        --numberCount;
+        ++i;
+        ++j;
+    }
+}
+ setNumbersInLotoCard("firstLotoCard");
+
+ setNumbersInLotoCard("secondLotoCard");
+
+ setNumbersInLotoCard("thirdLotoCard");
