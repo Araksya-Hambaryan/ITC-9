36b798e97a8e0b832d40dfcf6bef53e8a2c367a2
astghiksaghyan <astghiksaghyan@gmail.com>
2018-09-03 23:40:58 +0400

    Added logo

--- Astghik_Saghyan/Homeworks/Web/ReactJS/01_03.09.18/my-kitchen/src/Mainbar.js
+++ Astghik_Saghyan/Homeworks/Web/ReactJS/01_03.09.18/my-kitchen/src/Mainbar.js
@@ -5,14 +5,17 @@ class Mainbar extends Component {
   render() {
     return (
       <div>
-        <div id="temporery">    
-            <ul>
-                <li><a>PROFILE</a></li>
-                <li><a>ABOUT</a></li>
-                <li><a>PAYMENT HISTORY</a></li>
-                <li><a>PAYMENT</a></li>
-                <li><a className="active">CURRENT CONSULTANT</a></li>
-            </ul>
+        <div id="temporery">  
+            <div id="menu">
+                <img id="logo" src={require('./image/logo.png')} alt="Logo"></img>
+                <ul>
+                    <li><a>PROFILE</a></li>
+                    <li><a>ABOUT</a></li>
+                    <li><a>PAYMENT HISTORY</a></li>
+                    <li><a>PAYMENT</a></li>
+                    <li><a className="active">CURRENT CONSULTANT</a></li>
+                </ul>
+            </div>
         </div>    
       </div>
     );
