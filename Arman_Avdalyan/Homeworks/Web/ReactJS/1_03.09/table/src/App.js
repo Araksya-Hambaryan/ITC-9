a4bea45e5d90f46bc1433d8b0fc8c2aa68001bb2
Arman <avdalyan1999@gmail.com>
2018-09-04 23:17:53 +0400

    Added list task

--- Arman_Avdalyan/Homeworks/Web/ReactJS/1_03.09/table/src/App.js
+++ Arman_Avdalyan/Homeworks/Web/ReactJS/1_03.09/table/src/App.js
@@ -1,73 +1,110 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var createReactClass = require('create-react-class');
 
-var columns = ['Date', 'Daily', 'Total'];
 
-var data =
-  [{
-    'Date': '01.10.18',
-    'Daily': 500,
-    'Total': 12000
-  }, {
-    'Date': '01.11.18',
-    'Daily': 500,
-    'Total': 13000
-  }, {
-    'Date': '01.12.18',
-    'Daily': 600,
-    'Total': 18000
-  }];
-
-class App extends Component {
-  render() {
+var App = createReactClass({
+  getInitialState: function () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      {
+        rows: [{
+          'Date': '01.09.2018',
+          'Daily expences': '500',
+          'Payment': 50,
+          'Total expences': '12500',
+        }, {
+          'Date': '01.09.2018',
+          'Daily expences': '500',
+          'Payment': 50,
+          'Total expences': '13500',
+        }, {
+          'Date': '01.09.2018',
+          'Daily expences': '500',
+          'Payment': 50,
+          'Total expences': '14500',
+        }]
+      }
+    )
+  },
+  addRow: function (row) {
+    var timestamp = (new Date()).getTime();
+    this.state.rows['row-' + timestamp] = row;
+    this.setState({ rows: this.state.rows });
+  },
+  render: function () {
+    return (
+      <div className="component-wrapper">
+        <TableComponent rows={this.state.rows} />
+        <AddRowForm addRow={this.addRow} />
+      </div>
+    );
+  }
+});
+
+var columns = ['Date', 'Daily expences', 'Payment', 'Total expences'];
+
+var TableComponent = createReactClass({
+  render: function () {
+    var dataColumns = columns;
+    var dataRows = this.props.rows;
+    var tableColumns = (<thead>
+      <tr>
+        {dataColumns.map(function (column) {
+          return <td>{column}</td>;
+        })}
+      </tr>
+    </thead>);
+
+    var tableBody = dataRows.map(function (row) {
+      return (
+        <tbody>
+        <tr>
+          {dataColumns.map(function (column) {
+            return <td>{row[column]}</td>;
+          })}
+        </tr>
+        </tbody>);
+    });
+    return (<table className="table table-bordered table-hover" width="100%">
+      {tableColumns}
+      {tableBody}
+    </table>)
+  }
+});
+
+
+var AddRowForm = createReactClass({
+  createRow: function (e) {
+    e.preventDefault();
+    var row = this.refs.row.value;
+    if (typeof row === 'string' && row.length > 0) {
+      let arr = row.split(":");
+      let obj = {
+        'Date': arr[0],
+        'Daily expences': arr[1],
+        'Payment': arr[2],
+        'Total expences': arr[3],
+      }
+      this.state.rows.push(obj);
+      this.props.addRow(row);
+      console.log(this.props.rows);
+      this.refs.rowForm.reset();
+    }
+  },
+  render: function () {
+    return (
+      <form className="form-inline" ref="rowForm" onSubmit={this.createFruit}>
+        <div className="form-group">
+          <label htmlFor="rowItem">
+            Input row(like this 04.09.2018:700:0:15000)
+              <input type="text" id="rowItem" ref="row" className="form-control" />
+          </label>
         </div>
-        <CustomTable></CustomTable>
-      </div>
-    );
+        <button type="submit" className="btn btn-primary">Add Fruit</button>
+      </form>
+    )
   }
-}
+});
 
-class CustomTable extends React.Component {
-  render() {
-    return (
-      <div>
-        <table>
-          <Columns></Columns>
-          <tbody>
-            <TableRow></TableRow>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-class Columns extends React.Component {
-  render() {
-    return (<tr>
-      {columns.map(function (column) {
-        return <td>{column}</td>;
-      })}
-    </tr>);
-  }
-}
-
-class TableRow extends React.Component {
-  render() {
-    return (<tr>
-      {data.map(function (row) {
-        return
-        row.map(function (item) {
-          <td>{item}</td>
-        });
-      })}
-    </tr>);
-  }
-}
 export default App;
