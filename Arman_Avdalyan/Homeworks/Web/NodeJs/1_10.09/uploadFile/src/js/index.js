14c1d8899194c37e0e23562e275fa6dd17e5feee
Arman <avdalyan1999@gmail.com>
2018-09-11 00:20:14 +0400

    Updated the second task

--- Arman_Avdalyan/Homeworks/Web/NodeJs/1_10.09/uploadFile/src/js/index.js
+++ Arman_Avdalyan/Homeworks/Web/NodeJs/1_10.09/uploadFile/src/js/index.js
@@ -2,39 +2,52 @@ var express = require('express');
 var formidable = require('formidable');
 const events = require('events');
 const path = require('path');
-var element = require('html-element');
-var document = element.document;
+var fs = require('fs');
+
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 const rootDir = path.dirname(path.dirname(__dirname));
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.sendFile(rootDir + '/public/index.html');
 });
 
 var filePath = null;
 
-app.post('/', function (req, res){
+app.post('/', function (req, res) {
     var form = new formidable.IncomingForm();
+    var eventEmitter = new events.EventEmitter();
+    var ext = null;
+    var fileName = null;
     form.parse(req);
-    form.on('fileBegin', function (name, file){
+    form.on('fileBegin', function (name, file) {
         file.path = rootDir + '/uploads/' + file.name;
         filePath = file.path;
     });
-    form.on('file', function (name, file){
+    form.on('file', function (name, file) {
         console.log('Uploaded ' + file.name);
-        var ext = path.extname('index.html')
+        ext = path.extname(file.name);
+        fileName = file.name;
+        console.log(fileName)
         eventEmitter.emit(ext);
     });
+    eventEmitter.on('.jpg', function () {
+        res.write('<html><body>\
+                    <img src="/home/arman/Desktop/git/ITC-9/Arman_Avdalyan/\
+                    Homeworks/Web/NodeJs/1_10.09/uploadFile/uploads/app_logo.jpg">\
+                </body></html>');
+        res.end();
+    });
+
+    eventEmitter.on('.txt', function () {
+        fs.readFile(fileName, function(err, data) {
+            if(err) {
+                console.log('Failed');
+                return;
+            }
+            res.end('<html><body>\
+                    <textarea ${data}\
+                </body></html>');
+        });
+    });
 }).listen(8081, () => console.log('App listening on port 8081!'));
-
-var eventEmitter = new events.EventEmitter();
-var elem = null;
-
-eventEmitter.on('.img', function(){
-    elem = document.createElement("h1");
-    element.appendChild(elem);
-});
-
-eventEmitter.on('.txt', function(){
-});
