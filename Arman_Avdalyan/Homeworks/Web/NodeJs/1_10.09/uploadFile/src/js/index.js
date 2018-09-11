a725559ad800222cc752617c827c7e999304cd92
Arman <avdalyan1999@gmail.com>
2018-09-11 21:00:38 +0400

    Updated file-upload task

--- Arman_Avdalyan/Homeworks/Web/NodeJs/1_10.09/uploadFile/src/js/index.js
+++ Arman_Avdalyan/Homeworks/Web/NodeJs/1_10.09/uploadFile/src/js/index.js
@@ -19,35 +19,49 @@ app.post('/', function (req, res) {
     var eventEmitter = new events.EventEmitter();
     var ext = null;
     var fileName = null;
+    
     form.parse(req);
     form.on('fileBegin', function (name, file) {
         file.path = rootDir + '/uploads/' + file.name;
         filePath = file.path;
     });
+
     form.on('file', function (name, file) {
         console.log('Uploaded ' + file.name);
         ext = path.extname(file.name);
         fileName = file.name;
-        console.log(fileName)
         eventEmitter.emit(ext);
     });
+
     eventEmitter.on('.jpg', function () {
-        res.write('<html><body>\
-                    <img src="/home/arman/Desktop/git/ITC-9/Arman_Avdalyan/\
-                    Homeworks/Web/NodeJs/1_10.09/uploadFile/uploads/app_logo.jpg">\
-                </body></html>');
-        res.end();
+        showImage();
     });
 
+    eventEmitter.on('.png', function () {
+        showImage();
+    });
+
+    function showImage() {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`<html><body>\
+                    <img src="../../uploads/Table.png">\
+                </body></html>`);
+        res.end();
+    }
+
     eventEmitter.on('.txt', function () {
-        fs.readFile(fileName, function(err, data) {
-            if(err) {
-                console.log('Failed');
-                return;
-            }
-            res.end('<html><body>\
-                    <textarea ${data}\
-                </body></html>');
+        fs.readFile(filePath, function (err, data) {
+            res.write(`<html><body>\
+                    <textarea> ${data.toString()}</textarea>\
+                </body></html>`);
+            res.end();
+        });
+    });
+
+    eventEmitter.on('.html', function () {
+        fs.readFile(filePath, function (err, data) {
+            res.write(data.toString());
+            res.end();
         });
     });
 }).listen(8081, () => console.log('App listening on port 8081!'));
