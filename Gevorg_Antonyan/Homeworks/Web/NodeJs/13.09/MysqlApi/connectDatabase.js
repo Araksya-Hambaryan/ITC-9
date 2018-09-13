4945071862df2bd3cb393cad35224b072b20fd38
GevorgAntonyan <gevorg.antonyan94@gmail.com>
2018-09-13 22:52:08 +0400

    added task

--- Gevorg_Antonyan/Homeworks/Web/NodeJs/13.09/MysqlApi/connectDatabase.js
+++ Gevorg_Antonyan/Homeworks/Web/NodeJs/13.09/MysqlApi/connectDatabase.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'gevor1994',
+    password: 'password',
     database: 'restauranesDB'
 });
 
